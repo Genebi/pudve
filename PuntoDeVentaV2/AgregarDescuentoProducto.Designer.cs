b171987496c7303c09bc333b707a8e75de08dd54
Genebi <genebi@outlook.com>
2019-01-26 12:26:48 -0600

    Modificación descuento por Cliente 1

--- PuntoDeVentaV2/AgregarDescuentoProducto.Designer.cs
+++ PuntoDeVentaV2/AgregarDescuentoProducto.Designer.cs
@@ -30,18 +30,19 @@
         {
             this.label8 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.btnCliente = new System.Windows.Forms.Button();
-            this.btnMayoreo = new System.Windows.Forms.Button();
             this.btnAceptarDesc = new System.Windows.Forms.Button();
             this.btnCancelarDesc = new System.Windows.Forms.Button();
             this.txtTituloDescuento = new System.Windows.Forms.Label();
             this.panelContenedor = new System.Windows.Forms.FlowLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rbCliente = new System.Windows.Forms.RadioButton();
+            this.rbMayoreo = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // label8
             // 
             this.label8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label8.Location = new System.Drawing.Point(17, 76);
+            this.label8.Location = new System.Drawing.Point(17, 64);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(750, 2);
             this.label8.TabIndex = 21;
@@ -53,38 +54,6 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(750, 2);
             this.label1.TabIndex = 22;
-            // 
-            // btnCliente
-            // 
-            this.btnCliente.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.btnCliente.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnCliente.FlatAppearance.BorderSize = 0;
-            this.btnCliente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCliente.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCliente.ForeColor = System.Drawing.Color.White;
-            this.btnCliente.Location = new System.Drawing.Point(102, 24);
-            this.btnCliente.Name = "btnCliente";
-            this.btnCliente.Size = new System.Drawing.Size(200, 35);
-            this.btnCliente.TabIndex = 23;
-            this.btnCliente.Text = "+ Descuento por Cliente";
-            this.btnCliente.UseVisualStyleBackColor = false;
-            this.btnCliente.Click += new System.EventHandler(this.btnCliente_Click);
-            // 
-            // btnMayoreo
-            // 
-            this.btnMayoreo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.btnMayoreo.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnMayoreo.FlatAppearance.BorderSize = 0;
-            this.btnMayoreo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnMayoreo.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMayoreo.ForeColor = System.Drawing.Color.White;
-            this.btnMayoreo.Location = new System.Drawing.Point(486, 24);
-            this.btnMayoreo.Name = "btnMayoreo";
-            this.btnMayoreo.Size = new System.Drawing.Size(200, 35);
-            this.btnMayoreo.TabIndex = 24;
-            this.btnMayoreo.Text = "+ Descuento por Mayoreo";
-            this.btnMayoreo.UseVisualStyleBackColor = false;
-            this.btnMayoreo.Click += new System.EventHandler(this.btnMayoreo_Click);
             // 
             // btnAceptarDesc
             // 
@@ -123,18 +92,52 @@
             // txtTituloDescuento
             // 
             this.txtTituloDescuento.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtTituloDescuento.Location = new System.Drawing.Point(17, 94);
+            this.txtTituloDescuento.Location = new System.Drawing.Point(13, 20);
             this.txtTituloDescuento.Name = "txtTituloDescuento";
-            this.txtTituloDescuento.Size = new System.Drawing.Size(748, 23);
+            this.txtTituloDescuento.Size = new System.Drawing.Size(754, 23);
             this.txtTituloDescuento.TabIndex = 28;
             this.txtTituloDescuento.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // panelContenedor
             // 
-            this.panelContenedor.Location = new System.Drawing.Point(17, 132);
+            this.panelContenedor.Location = new System.Drawing.Point(17, 80);
             this.panelContenedor.Name = "panelContenedor";
-            this.panelContenedor.Size = new System.Drawing.Size(748, 291);
+            this.panelContenedor.Size = new System.Drawing.Size(748, 280);
             this.panelContenedor.TabIndex = 29;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(15, 380);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(750, 2);
+            this.label2.TabIndex = 30;
+            // 
+            // rbCliente
+            // 
+            this.rbCliente.AutoSize = true;
+            this.rbCliente.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbCliente.Location = new System.Drawing.Point(191, 397);
+            this.rbCliente.Name = "rbCliente";
+            this.rbCliente.Size = new System.Drawing.Size(192, 24);
+            this.rbCliente.TabIndex = 31;
+            this.rbCliente.TabStop = true;
+            this.rbCliente.Text = "Descuento por Cliente";
+            this.rbCliente.UseVisualStyleBackColor = true;
+            this.rbCliente.CheckedChanged += new System.EventHandler(this.rbCliente_CheckedChanged);
+            // 
+            // rbMayoreo
+            // 
+            this.rbMayoreo.AutoSize = true;
+            this.rbMayoreo.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbMayoreo.Location = new System.Drawing.Point(400, 397);
+            this.rbMayoreo.Name = "rbMayoreo";
+            this.rbMayoreo.Size = new System.Drawing.Size(208, 24);
+            this.rbMayoreo.TabIndex = 32;
+            this.rbMayoreo.TabStop = true;
+            this.rbMayoreo.Text = "Descuento por Mayoreo";
+            this.rbMayoreo.UseVisualStyleBackColor = true;
+            this.rbMayoreo.CheckedChanged += new System.EventHandler(this.rbMayoreo_CheckedChanged);
             // 
             // AgregarDescuentoProducto
             // 
@@ -142,12 +145,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(780, 497);
+            this.Controls.Add(this.rbMayoreo);
+            this.Controls.Add(this.rbCliente);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.panelContenedor);
             this.Controls.Add(this.txtTituloDescuento);
             this.Controls.Add(this.btnAceptarDesc);
             this.Controls.Add(this.btnCancelarDesc);
-            this.Controls.Add(this.btnMayoreo);
-            this.Controls.Add(this.btnCliente);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label8);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
@@ -158,17 +162,19 @@
             this.Text = "PUDVE - Descuento Producto";
             this.Load += new System.EventHandler(this.AgregarDescuentoProducto_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnCliente;
-        private System.Windows.Forms.Button btnMayoreo;
         private System.Windows.Forms.Button btnAceptarDesc;
         private System.Windows.Forms.Button btnCancelarDesc;
         private System.Windows.Forms.Label txtTituloDescuento;
         private System.Windows.Forms.FlowLayoutPanel panelContenedor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rbCliente;
+        private System.Windows.Forms.RadioButton rbMayoreo;
     }
 }
--- PuntoDeVentaV2/AgregarDescuentoProducto.cs
+++ PuntoDeVentaV2/AgregarDescuentoProducto.cs
@@ -25,24 +25,13 @@ namespace PuntoDeVentaV2
             if (tipoDescuento == 1)
             {
                 txtTituloDescuento.Text = "Descuento por Cliente";
+                rbCliente.Checked = true;
             }
         }
 
         private void AgregarDescuentoProducto_Load(object sender, EventArgs e)
         {
             CargarFormularios(tipoDescuento);
-        }
-
-        private void btnCliente_Click(object sender, EventArgs e)
-        {
-            tipoDescuento = 1;
-            MessageBox.Show("Por cliente");
-        }
-
-        private void btnMayoreo_Click(object sender, EventArgs e)
-        {
-            tipoDescuento = 2;
-            MessageBox.Show("Por mayoreo");
         }
 
         private void btnCancelarDesc_Click(object sender, EventArgs e)
@@ -57,23 +46,96 @@ namespace PuntoDeVentaV2
 
         private void CargarFormularios(int tipo)
         {
+            panelContenedor.Controls.Clear();
+
             //Descuento por cliente
             if (tipo == 1)
             {
                 FlowLayoutPanel panelHijo = new FlowLayoutPanel();
-                panelHijo.Name = "panelGeneradoCliente";
+                panelHijo.Name = "panelGeneradoCliente1";
                 panelHijo.Width = 745;
                 panelHijo.Height = 290;
 
-                TextBox tbPrecio = new TextBox();
-                tbPrecio.Name = "txtPrecio";
-                tbPrecio.Width = 100;
-                tbPrecio.Height = 20;
-                tbPrecio.Margin = new Padding(20, 0, 0, 0);
-                tbPrecio.TextAlign = HorizontalAlignment.Center;
+                Label lb1 = new Label();
+                lb1.Text = "Precio de producto";
+                lb1.Margin = new Padding(270, 10, 0, 0);
+                lb1.Font = new Font("Century Gothic", 11);
+                lb1.AutoSize = false;
+                lb1.Width = 220;
+                lb1.Height = 20;
+                lb1.TextAlign = ContentAlignment.MiddleCenter;
 
-                panelHijo.Controls.Add(tbPrecio);
-                panelHijo.FlowDirection = FlowDirection.LeftToRight;
+                TextBox tb1 = new TextBox();
+                tb1.Name = "txtPrecio";
+                tb1.Width = 220;
+                tb1.Height = 20;
+                tb1.Margin = new Padding(270, 5, 0, 0);
+                tb1.TextAlign = HorizontalAlignment.Center;
+                tb1.Enabled = false;
+                tb1.BackColor = Color.White;
+                
+
+                Label lb2 = new Label();
+                lb2.Text = "% de Descuento";
+                lb2.AutoSize = false;
+                lb2.Width = 220;
+                lb2.Height = 20;
+                lb2.Margin = new Padding(270, 20, 0, 0);
+                lb2.Font = new Font("Century Gothic", 11);
+                lb2.TextAlign = ContentAlignment.MiddleCenter;
+
+                TextBox tb2 = new TextBox();
+                tb2.Name = "txtPorcentaje";
+                tb2.Width = 220;
+                tb2.Height = 20;
+                tb2.Margin = new Padding(270, 5, 0, 0);
+                tb2.TextAlign = HorizontalAlignment.Center;
+
+                Label lb3 = new Label();
+                lb3.Text = "Precio con Descuento";
+                lb3.AutoSize = false;
+                lb3.Width = 220;
+                lb3.Height = 20;
+                lb3.Margin = new Padding(270, 20, 0, 0);
+                lb3.Font = new Font("Century Gothic", 11);
+                lb3.TextAlign = ContentAlignment.MiddleCenter;
+
+                TextBox tb3 = new TextBox();
+                tb3.Name = "txtPrecioDescuento";
+                tb3.Width = 220;
+                tb3.Height = 20;
+                tb3.Margin = new Padding(270, 5, 0, 0);
+                tb3.TextAlign = HorizontalAlignment.Center;
+                tb3.Enabled = false;
+                tb3.BackColor = Color.White;
+
+                Label lb4 = new Label();
+                lb4.Text = "Descuento";
+                lb4.AutoSize = false;
+                lb4.Width = 220;
+                lb4.Height = 20;
+                lb4.Margin = new Padding(270, 20, 0, 0);
+                lb4.Font = new Font("Century Gothic", 11);
+                lb4.TextAlign = ContentAlignment.MiddleCenter;
+
+                TextBox tb4 = new TextBox();
+                tb4.Name = "txtDescuento";
+                tb4.Width = 220;
+                tb4.Height = 20;
+                tb4.Margin = new Padding(270, 5, 0, 0);
+                tb4.TextAlign = HorizontalAlignment.Center;
+                tb4.Enabled = false;
+                tb4.BackColor = Color.White;
+
+                panelHijo.Controls.Add(lb1);
+                panelHijo.Controls.Add(tb1);
+                panelHijo.Controls.Add(lb2);
+                panelHijo.Controls.Add(tb2);
+                panelHijo.Controls.Add(lb3);
+                panelHijo.Controls.Add(tb3);
+                panelHijo.Controls.Add(lb4);
+                panelHijo.Controls.Add(tb4);
+                panelHijo.FlowDirection = FlowDirection.TopDown;
 
                 panelContenedor.Controls.Add(panelHijo);
                 panelContenedor.FlowDirection = FlowDirection.TopDown;
@@ -84,6 +146,18 @@ namespace PuntoDeVentaV2
             {
 
             }
+        }
+
+        private void rbCliente_CheckedChanged(object sender, EventArgs e)
+        {
+            tipoDescuento = 1;
+            CargarFormularios(tipoDescuento);
+        }
+
+        private void rbMayoreo_CheckedChanged(object sender, EventArgs e)
+        {
+            tipoDescuento = 2;
+            CargarFormularios(tipoDescuento);
         }
     }
 }
