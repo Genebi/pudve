428c0840869ea5e8a8fb7fa8dd76d5c8ed291eff
Genebi <genebi@outlook.com>
2019-04-16 12:27:17 -0500

    Commit de emergencia

---
+++ PuntoDeVentaV2/ListadoAnticipos.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace PuntoDeVentaV2
+{
+    partial class ListadoAnticipos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tituloSeccion = new System.Windows.Forms.Label();
+            this.DGVListaAnticipos = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Concepto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Importe = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cliente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Fecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Aplicar = new System.Windows.Forms.DataGridViewImageColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVListaAnticipos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tituloSeccion
+            // 
+            this.tituloSeccion.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.tituloSeccion.AutoSize = true;
+            this.tituloSeccion.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tituloSeccion.Location = new System.Drawing.Point(134, 24);
+            this.tituloSeccion.Name = "tituloSeccion";
+            this.tituloSeccion.Size = new System.Drawing.Size(263, 25);
+            this.tituloSeccion.TabIndex = 6;
+            this.tituloSeccion.Text = "ANTICIPOS POR APLICAR";
+            this.tituloSeccion.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // DGVListaAnticipos
+            // 
+            this.DGVListaAnticipos.AllowUserToAddRows = false;
+            this.DGVListaAnticipos.AllowUserToDeleteRows = false;
+            this.DGVListaAnticipos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVListaAnticipos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Concepto,
+            this.Importe,
+            this.Cliente,
+            this.Fecha,
+            this.Aplicar});
+            this.DGVListaAnticipos.Location = new System.Drawing.Point(12, 71);
+            this.DGVListaAnticipos.Name = "DGVListaAnticipos";
+            this.DGVListaAnticipos.ReadOnly = true;
+            this.DGVListaAnticipos.RowHeadersVisible = false;
+            this.DGVListaAnticipos.Size = new System.Drawing.Size(510, 178);
+            this.DGVListaAnticipos.TabIndex = 7;
+            this.DGVListaAnticipos.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGVListaAnticipos_CellClick);
+            this.DGVListaAnticipos.CellMouseEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGVListaAnticipos_CellMouseEnter);
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            this.ID.Visible = false;
+            this.ID.Width = 50;
+            // 
+            // Concepto
+            // 
+            this.Concepto.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Concepto.HeaderText = "Concepto";
+            this.Concepto.Name = "Concepto";
+            this.Concepto.ReadOnly = true;
+            // 
+            // Importe
+            // 
+            this.Importe.HeaderText = "Importe";
+            this.Importe.Name = "Importe";
+            this.Importe.ReadOnly = true;
+            this.Importe.Width = 50;
+            // 
+            // Cliente
+            // 
+            this.Cliente.HeaderText = "Cliente";
+            this.Cliente.Name = "Cliente";
+            this.Cliente.ReadOnly = true;
+            this.Cliente.Width = 105;
+            // 
+            // Fecha
+            // 
+            this.Fecha.HeaderText = "Fecha";
+            this.Fecha.Name = "Fecha";
+            this.Fecha.ReadOnly = true;
+            this.Fecha.Width = 110;
+            // 
+            // Aplicar
+            // 
+            this.Aplicar.HeaderText = "";
+            this.Aplicar.Name = "Aplicar";
+            this.Aplicar.ReadOnly = true;
+            this.Aplicar.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Aplicar.Width = 50;
+            // 
+            // ListadoAnticipos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 261);
+            this.Controls.Add(this.DGVListaAnticipos);
+            this.Controls.Add(this.tituloSeccion);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "ListadoAnticipos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PUDVE - Lista de Anticipos";
+            ((System.ComponentModel.ISupportInitialize)(this.DGVListaAnticipos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label tituloSeccion;
+        private System.Windows.Forms.DataGridView DGVListaAnticipos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Concepto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Importe;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cliente;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fecha;
+        private System.Windows.Forms.DataGridViewImageColumn Aplicar;
+    }
+}
---
+++ PuntoDeVentaV2/ListadoAnticipos.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PuntoDeVentaV2
+{
+    public partial class ListadoAnticipos : Form
+    {
+        Conexion cn = new Conexion();
+        Consultas cs = new Consultas();
+
+        string[] anticipos = new string[] { };
+
+        public ListadoAnticipos()
+        {
+            InitializeComponent();
+            CargarDatos();
+        }
+
+        private void CargarDatos()
+        {
+            SQLiteConnection sql_con;
+            SQLiteCommand sql_cmd;
+            SQLiteDataReader dr;
+
+            sql_con = new SQLiteConnection("Data source=" + Properties.Settings.Default.rutaDirectorio + "\\BD\\pudveDB.db; Version=3; New=False;Compress=True;");
+            sql_con.Open();
+            sql_cmd = new SQLiteCommand($"SELECT * FROM Anticipos WHERE IDUsuario = {FormPrincipal.userID} AND Status = 1", sql_con);
+            dr = sql_cmd.ExecuteReader();
+
+            DGVListaAnticipos.Rows.Clear();
+
+            //Si la variable no esta vacia eliminamos el ultimo elemento de la cadena "-"
+            //Convertimos la cadena en Array y se asigna al Array anticipos
+            if (Ventas.listaAnticipos != "")
+            {
+                var auxiliar = Ventas.listaAnticipos.Remove(Ventas.listaAnticipos.Length - 1);
+
+                var cadena = auxiliar.Split('-');
+
+                anticipos = cadena;
+            }
+
+            while (dr.Read())
+            {
+                
+                int rowId = DGVListaAnticipos.Rows.Add();
+
+                DataGridViewRow row = DGVListaAnticipos.Rows[rowId];
+
+                row.Cells["ID"].Value = dr.GetValue(dr.GetOrdinal("ID"));
+                row.Cells["Concepto"].Value = dr.GetValue(dr.GetOrdinal("Concepto"));
+                row.Cells["Importe"].Value = dr.GetValue(dr.GetOrdinal("Importe"));
+                row.Cells["Cliente"].Value = dr.GetValue(dr.GetOrdinal("Cliente"));  
+                row.Cells["Fecha"].Value = Convert.ToDateTime(dr.GetValue(dr.GetOrdinal("Fecha"))).ToString("yyyy-MM-dd HH:mm:ss");
+
+                int seleccionado = Array.IndexOf(anticipos, dr.GetValue(dr.GetOrdinal("ID")).ToString());
+
+                //Si ya fue seleccionado anteriormente agregamos un icono diferente al momento de visualizarlos
+                if (seleccionado > -1)
+                {
+                    Image aplicar = Image.FromFile(Properties.Settings.Default.rutaDirectorio + @"\icon\black16\ban.png");
+
+                    row.Cells["Aplicar"].Value = aplicar;
+
+                    row.Cells["Aplicar"].ToolTipText = "Ya fue aplicado";
+                }
+                else
+                {
+                    Image aplicar = Image.FromFile(Properties.Settings.Default.rutaDirectorio + @"\icon\black16\reply.png");
+
+                    row.Cells["Aplicar"].Value = aplicar;
+
+                    row.Cells["Aplicar"].ToolTipText = "Aplicar";
+                }
+            }
+
+            DGVListaAnticipos.ClearSelection();
+
+            dr.Close();
+            sql_con.Close();
+        }
+
+        private void DGVListaAnticipos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //Boton Aplicar
+            if (e.ColumnIndex == 5)
+            {
+                var fila = DGVListaAnticipos.CurrentCell.RowIndex;
+
+                int idAnticipo = Convert.ToInt32(DGVListaAnticipos.Rows[fila].Cells["ID"].Value);
+
+                int seleccionado = Array.IndexOf(anticipos, idAnticipo.ToString());
+
+                //Si el anticipo no ha sido aplicado se hace la operacion normalmente
+                if (seleccionado < 0)
+                {
+                    Ventas.listaAnticipos += idAnticipo + "-";
+
+                    Ventas.importeAnticipo = float.Parse(DGVListaAnticipos.Rows[fila].Cells["Importe"].Value.ToString());
+
+                    this.Close();
+                }
+            }
+
+            DGVListaAnticipos.ClearSelection();
+        }
+
+        private void DGVListaAnticipos_CellMouseEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                //Aplicar
+                if (e.ColumnIndex == 5)
+                {
+                    DGVListaAnticipos.Cursor = Cursors.Hand;
+                }
+                else
+                {
+                    DGVListaAnticipos.Cursor = Cursors.Default;
+                }
+            }
+        }
+    }
+}
