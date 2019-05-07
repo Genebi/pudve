e6fa5a2be9b077ce002820f8464a815302272006
Emmanuel Ruiz Garcia <emmanuel.ruiz.garcia@outlook.com>
2019-05-07 12:09:58 -0500

    Acomodo del ComboBox muestra el dato

---
+++ PuntoDeVentaV2/ItemsProductoComboBox.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuntoDeVentaV2
+{
+    public class ItemsProductoComboBox
+    {
+        string _text;
+        string _value;
+
+        public ItemsProductoComboBox()
+        {
+            _text = string.Empty;
+            _value = string.Empty;
+        }
+
+        public ItemsProductoComboBox(string text, string value)
+        {
+            _text = text;
+            _value = value;
+        }
+
+        public string text
+        {
+            get
+            {
+                return _text;
+            }
+            set
+            {
+                _text = value;
+            }
+        }
+
+        public string Value
+        {
+            get { return _value; }
+            set { _value = value; }
+        }
+
+        public override string ToString()
+        {
+            return _value;
+        }
+    }
+}
