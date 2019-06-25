2675c85f6445256f023440ddbae9b2ff451bad69
Emmanuel <emmanuel.ruiz.garcia@outlook.com>
2019-06-25 09:41:15 -0500

    Configuracion de Path's Archivos DB etc., para instalador

--- PuntoDeVentaV2/Login.cs
+++ PuntoDeVentaV2/Login.cs
@@ -13,6 +13,34 @@ namespace PuntoDeVentaV2
 {
     public partial class Login : Form
     {
+        private string _pathAssets = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\PUDVE\assets\";
+        private string _pathBarCode = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\PUDVE\BarCode\";
+        private string _pathBD = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\PUDVE\BD\";
+        private string _pathIconBlack = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\PUDVE\icon\black\";
+        private string _pathIconBlack16 = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\PUDVE\icon\black16\";
+        private string _pathMisDatos = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\PUDVE\MisDatos\Usuarios\";
+        private string _pathPdfCode = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\PUDVE\PdfCode\";
+        private string _pathPdfTag = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\PUDVE\PdfTag\";
+        private string _pathProductos = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\PUDVE\Productos\";
+        private string _pathSetCodBar = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\PUDVE\settings\codbar\";
+        private string _pathSetFolioVenta = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\PUDVE\settings\folioventa\";
+        private string _pathSetGanancia = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\PUDVE\settings\ganancia\";
+
+        private string _Assets = Directory.GetCurrentDirectory() + @"\PUDVE\assets\";
+        private string _BarCode = Directory.GetCurrentDirectory() + @"\PUDVE\BarCode\";
+        private string _BD = Directory.GetCurrentDirectory() + @"\PUDVE\BD\";
+        private string _IconBlack = Directory.GetCurrentDirectory() + @"\PUDVE\icon\black\";
+        private string _IconBlack16 = Directory.GetCurrentDirectory() + @"\PUDVE\icon\black16\";
+        private string _MisDatos = Directory.GetCurrentDirectory() + @"\PUDVE\MisDatos\Usuarios\";
+        private string _PdfCode = Directory.GetCurrentDirectory() + @"\PUDVE\PdfCode\";
+        private string _PdfTag = Directory.GetCurrentDirectory() + @"\PUDVE\PdfTag\";
+        private string _Productos = Directory.GetCurrentDirectory() + @"\PUDVE\Productos\";
+        private string _SetCodBar = Directory.GetCurrentDirectory() + @"\PUDVE\settings\codbar\";
+        private string _SetFolioVenta = Directory.GetCurrentDirectory() + @"\PUDVE\settings\folioventa\";
+        private string _SetGanancia = Directory.GetCurrentDirectory() + @"\PUDVE\settings\ganancia\";
+
+        string[] pathsOrigen, pathsDestino;
+
         //public string rutaLocal = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
         Conexion cn = new Conexion();
 
@@ -113,8 +141,88 @@ namespace PuntoDeVentaV2
             this.Close();
         }
 
+        private void createDir(string[] path)
+        {
+            try
+            {
+                for (int x = 1; x <= path.Length; x++)
+                {
+                    if (!Directory.Exists(path[x - 1]))    // verificamos que si no existe el directorio
+                    {
+                        Directory.CreateDirectory(path[x - 1]);    // lo crea para poder almacenar los archivos
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al crear el directorio error: " + ex.Message, "Error en el Directorio", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void CopyWithProgress(string pathOrigen, string pathDestino)
+        {
+            DirectoryInfo source = new DirectoryInfo(pathOrigen);
+            FileInfo[] filesToCopy = source.GetFiles();
+
+            try
+            {
+                // Loop through all files to copy.
+                for (int x = 1; x <= filesToCopy.Length; x++)
+                {
+                    if (!File.Exists(pathDestino + filesToCopy[x - 1].ToString()))
+                    {
+                        File.Copy(pathOrigen + filesToCopy[x - 1].ToString(), pathDestino + filesToCopy[x - 1].ToString(), true);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al copiar error: " + ex.Message, "Error al copiar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
         private void Login_Load(object sender, EventArgs e)
         {
+            pathsOrigen = new string[] {_Assets,
+                                        _BarCode,
+                                        _BD,
+                                        _IconBlack,
+                                        _IconBlack16,
+                                        _MisDatos,
+                                        _PdfCode,
+                                        _PdfTag,
+                                        _Productos,
+                                        _SetCodBar,
+                                        _SetFolioVenta,
+                                        _SetGanancia };
+
+            pathsDestino = new string[] {   _pathAssets,
+                                            _pathBarCode,
+                                            _pathBD,
+                                            _pathIconBlack,
+                                            _pathIconBlack16,
+                                            _pathMisDatos,
+                                            _pathPdfCode,
+                                            _pathPdfTag,
+                                            _pathProductos,
+                                            _pathSetCodBar,
+                                            _pathSetFolioVenta,
+                                            _pathSetGanancia };
+
+            createDir(pathsDestino);
+
+            try
+            {
+                for (int i = 0; i < pathsOrigen.Length; i++)
+                {
+                    CopyWithProgress(pathsOrigen[i], pathsDestino[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al enviar parametro error: " + ex.Message, "Error al pasar parametro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
             var pathPUDVESistema = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
 
             // Varaiable para cuando hacemos el instalador
