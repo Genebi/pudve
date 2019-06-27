1b404107ec51f89c3ee7c45e5d5291552ef61452
Genebi <genebi@outlook.com>
2019-06-27 16:15:51 -0500

    Cambios para calcular el cambio

---
+++ PuntoDeVentaV2/ListaClientes.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace PuntoDeVentaV2
+{
+    partial class ListaClientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DGVClientes = new System.Windows.Forms.DataGridView();
+            this.RFC = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RazonSocial = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Agregar = new System.Windows.Forms.DataGridViewImageColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVClientes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DGVClientes
+            // 
+            this.DGVClientes.AllowUserToAddRows = false;
+            this.DGVClientes.AllowUserToDeleteRows = false;
+            this.DGVClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVClientes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.RFC,
+            this.RazonSocial,
+            this.Agregar});
+            this.DGVClientes.Location = new System.Drawing.Point(2, 1);
+            this.DGVClientes.Name = "DGVClientes";
+            this.DGVClientes.ReadOnly = true;
+            this.DGVClientes.RowHeadersVisible = false;
+            this.DGVClientes.Size = new System.Drawing.Size(481, 257);
+            this.DGVClientes.TabIndex = 0;
+            this.DGVClientes.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGVClientes_CellClick);
+            this.DGVClientes.CellMouseEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGVClientes_CellMouseEnter);
+            this.DGVClientes.CellMouseLeave += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGVClientes_CellMouseLeave);
+            // 
+            // RFC
+            // 
+            this.RFC.HeaderText = "RFC";
+            this.RFC.Name = "RFC";
+            this.RFC.ReadOnly = true;
+            this.RFC.Width = 150;
+            // 
+            // RazonSocial
+            // 
+            this.RazonSocial.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.RazonSocial.HeaderText = "Razón Social";
+            this.RazonSocial.Name = "RazonSocial";
+            this.RazonSocial.ReadOnly = true;
+            // 
+            // Agregar
+            // 
+            this.Agregar.Description = "Seleccionar";
+            this.Agregar.HeaderText = "";
+            this.Agregar.Name = "Agregar";
+            this.Agregar.ReadOnly = true;
+            this.Agregar.Width = 50;
+            // 
+            // ListaClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 261);
+            this.Controls.Add(this.DGVClientes);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "ListaClientes";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PUDVE - Lista de Clientes";
+            this.Load += new System.EventHandler(this.ListaClientes_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGVClientes)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DGVClientes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RFC;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RazonSocial;
+        private System.Windows.Forms.DataGridViewImageColumn Agregar;
+    }
+}
