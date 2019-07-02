5150d25e2aa2572a5efc352d983489439c003358
Emmanuel <emmanuel.ruiz.garcia@outlook.com>
2019-07-02 15:02:53 -0500

    Mostrar Stock ya Verificado en ventana Modal

--- PuntoDeVentaV2/ReporteFinalRevisarInventario.Designer.cs
+++ PuntoDeVentaV2/ReporteFinalRevisarInventario.Designer.cs
@@ -28,23 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.DGVRevisionStock = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVRevisionStock)).BeginInit();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.DGVRevisionStock);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(851, 245);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = " Reporte Final del Día: ";
+            // 
+            // DGVRevisionStock
+            // 
+            this.DGVRevisionStock.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVRevisionStock.Location = new System.Drawing.Point(23, 30);
+            this.DGVRevisionStock.Name = "DGVRevisionStock";
+            this.DGVRevisionStock.Size = new System.Drawing.Size(805, 192);
+            this.DGVRevisionStock.TabIndex = 0;
             // 
             // ReporteFinalRevisarInventario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(787, 345);
+            this.ClientSize = new System.Drawing.Size(875, 464);
+            this.Controls.Add(this.groupBox1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "ReporteFinalRevisarInventario";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Reporte Final de Revisar Inventario Fisico";
+            this.Load += new System.EventHandler(this.ReporteFinalRevisarInventario_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DGVRevisionStock)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView DGVRevisionStock;
     }
 }
--- PuntoDeVentaV2/ReporteFinalRevisarInventario.cs
+++ PuntoDeVentaV2/ReporteFinalRevisarInventario.cs
@@ -12,9 +12,49 @@ namespace PuntoDeVentaV2
 {
     public partial class ReporteFinalRevisarInventario : Form
     {
+        Conexion cn = new Conexion();
+        DataTable dtFinalReportCheckStockToDay;
+
+        bool IsEmpty;
+        string queryFiltroReporteStock, tabla;
+
         public ReporteFinalRevisarInventario()
         {
             InitializeComponent();
+        }
+
+        private void ReporteFinalRevisarInventario_Load(object sender, EventArgs e)
+        {
+            IsEmpty = false;
+            tabla = "RevisarInventario";
+            cargarTabla();
+            checkEmpty(tabla);
+            llenarDataGriView();
+        }
+
+        private void llenarDataGriView()
+        {
+            if (IsEmpty == true)
+            {
+                DGVRevisionStock.DataSource = dtFinalReportCheckStockToDay;
+            }
+            else if (IsEmpty == false)
+            {
+                MessageBox.Show("La base de datos de Checar Stock verificado no tiene registros", "No tiene registros", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void cargarTabla()
+        {
+            queryFiltroReporteStock = $"SELECT * FROM '{tabla}' WHERE StatusRevision = '1'";
+            dtFinalReportCheckStockToDay = cn.CargarDatos(queryFiltroReporteStock);
+        }
+
+        private bool checkEmpty(string tabla)
+        {
+            string queryTableCheck = $"SELECT * FROM '{tabla}'";
+            IsEmpty = cn.IsEmptyTable(queryTableCheck);
+            return IsEmpty;
         }
     }
 }
