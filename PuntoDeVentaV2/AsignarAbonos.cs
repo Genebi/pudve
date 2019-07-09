9e4396daf2461e47577fe4bf079c07bf025bdc04
Genebi <genebi@outlook.com>
2019-07-09 15:24:47 -0500

    Método para generar el ticket de abono

--- PuntoDeVentaV2/AsignarAbonos.cs
+++ PuntoDeVentaV2/AsignarAbonos.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -102,7 +105,7 @@ namespace PuntoDeVentaV2
             //Condicion para saber si se termino de pagar y cambiar el status de la venta
             if (totalAbonado >= totalPendiente)
             {
-                cn.EjecutarConsulta(cs.ActualizarVenta(idVenta, 1, FormPrincipal.userID));
+                //cn.EjecutarConsulta(cs.ActualizarVenta(idVenta, 1, FormPrincipal.userID));
             }
 
             string[] datos = new string[] {
@@ -110,12 +113,15 @@ namespace PuntoDeVentaV2
                 vales.ToString(), cheque.ToString(), transferencia.ToString(), referencia, fechaOperacion
             };
 
-            int resultado = cn.EjecutarConsulta(cs.GuardarAbonos(datos));
+            string[] tmp = new string[] { idVenta.ToString(), totalOriginal.ToString(), totalPendiente.ToString(), totalAbonado.ToString(), fechaOperacion };
+
+            GenerarTicket(tmp);
+            /*int resultado = cn.EjecutarConsulta(cs.GuardarAbonos(datos));
 
             if (resultado > 0)
             {
                 this.Dispose();
-            }
+            }*/
         }
 
         private void SoloDecimales(object sender, KeyPressEventArgs e)
@@ -243,6 +249,117 @@ namespace PuntoDeVentaV2
         private void txtEfectivo_KeyUp(object sender, KeyEventArgs e)
         {
             CalcularCambio();
+        }
+
+        private void GenerarTicket(string[] info)
+        {
+            var datos = FormPrincipal.datosUsuario;
+
+            //Medidas de ticket de 57 y 80 mm
+            //57mm = 161.28 pt
+            //80mm = 226.08 pt
+
+            var tipoPapel = 57;
+            var anchoPapel = Convert.ToInt32(Math.Floor((((tipoPapel * 0.10) * 72) / 2.54)));
+            var altoPapel = Convert.ToInt32(anchoPapel + 54);
+
+            string salto = string.Empty;
+
+            int medidaFuenteMensaje = 0;
+            int medidaFuenteNegrita = 0;
+            int medidaFuenteNormal = 0;
+            int medidaFuenteGrande = 0;
+
+            int separadores = 0;
+            int anchoLogo = 0;
+            int altoLogo = 0;
+            int espacio = 0;
+
+            if (tipoPapel == 80)
+            {
+                separadores = 81;
+                anchoLogo = 110;
+                altoLogo = 60;
+                espacio = 10;
+
+                medidaFuenteMensaje = 10;
+                medidaFuenteGrande = 10;
+                medidaFuenteNegrita = 8;
+                medidaFuenteNormal = 8;
+
+                salto = "\n";
+            }
+            else if (tipoPapel == 57)
+            {
+                separadores = 75;
+                anchoLogo = 80;
+                altoLogo = 40;
+                espacio = 8;
+
+                medidaFuenteMensaje = 6;
+                medidaFuenteGrande = 8;
+                medidaFuenteNegrita = 6;
+                medidaFuenteNormal = 6;
+
+                salto = string.Empty;
+            }
+
+            Document ticket = new Document(new iTextSharp.text.Rectangle(anchoPapel, altoPapel), 3, 3, 5, 0);
+            PdfWriter writer = PdfWriter.GetInstance(ticket, new FileStream(@"C:\Archivos PUDVE\Ventas\Tickets\ticket_abono_" + info[0] + ".pdf", FileMode.Create));
+
+            var fuenteNormal = FontFactory.GetFont(FontFactory.HELVETICA, medidaFuenteNormal);
+            var fuenteNegrita = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, medidaFuenteNegrita);
+            var fuenteGrande = FontFactory.GetFont(FontFactory.HELVETICA, medidaFuenteGrande);
+            var fuenteMensaje = FontFactory.GetFont(FontFactory.HELVETICA, medidaFuenteMensaje);
+
+            string logotipo = @"C:\Archivos PUDVE\MisDatos\Usuarios\" + datos[11];
+            string encabezado = $"{salto}{datos[1]} {datos[2]} {datos[3]}, {datos[4]}, {datos[5]}\nCol. {datos[6]} C.P. {datos[7]}\nRFC: {datos[8]}\n{datos[9]}\nTel. {datos[10]}\n\n";
+
+            ticket.Open();
+
+            //Validación para verificar si existe logotipo
+            if (logotipo != "")
+            {
+                if (File.Exists(logotipo))
+                {
+                    iTextSharp.text.Image logo = iTextSharp.text.Image.GetInstance(logotipo);
+                    logo.Alignment = iTextSharp.text.Image.ALIGN_CENTER;
+                    logo.ScaleAbsolute(anchoLogo, altoLogo);
+                    ticket.Add(logo);
+                }
+            }
+
+            Paragraph titulo = new Paragraph(datos[0] + "\n", fuenteGrande);
+            Paragraph domicilio = new Paragraph(encabezado, fuenteNormal);
+
+            titulo.Alignment = Element.ALIGN_CENTER;
+            domicilio.Alignment = Element.ALIGN_CENTER;
+            domicilio.SetLeading(espacio, 0);
+
+            Paragraph separadorInicial = new Paragraph(new string('-', separadores), fuenteNormal);
+
+            //Contenido del Ticket
+            string contenido = $"ID de venta: {info[0]}\nTotal original: {info[1]}\nPendiente de pago: {info[2]}\nCantidad abonada: ${info[3]}\n{info[4]}";
+
+            Paragraph cuerpo = new Paragraph(contenido, fuenteNormal);
+            cuerpo.Alignment = Element.ALIGN_CENTER;
+
+            Paragraph mensaje = new Paragraph("Comprobante de Abono.", fuenteGrande);
+            mensaje.Alignment = Element.ALIGN_CENTER;
+
+            Paragraph separadorFinal = new Paragraph(new string('-', separadores), fuenteNormal);
+
+            ticket.Add(titulo);
+            ticket.Add(domicilio);
+            ticket.Add(separadorInicial);
+            ticket.Add(cuerpo);
+            ticket.Add(separadorFinal);
+            ticket.Add(mensaje);
+
+            ticket.AddTitle("Ticket Abono");
+            ticket.AddAuthor("PUDVE");
+            ticket.Close();
+            writer.Close();
         }
     }
 }
