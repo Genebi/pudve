747481dd8e8aeb56ed033a28da627322377df023
Emmanuel <emmanuel.ruiz.garcia@outlook.com>
2019-07-30 10:02:51 -0500

    Mostrar Proveedor si es que lo tiene el Producto o Servicio

--- PuntoDeVentaV2/AgregarEditarProducto.cs
+++ PuntoDeVentaV2/AgregarEditarProducto.cs
@@ -2443,18 +2443,46 @@ namespace PuntoDeVentaV2
         private void btnDetalleProducto_Click(object sender, EventArgs e)
         {
             FormDetalleProducto = Application.OpenForms.OfType<AgregarDetalleProducto>().FirstOrDefault();
-            
-            //Verifica que el formulario ya tenga una instancia creada, de lo contrario la crea
-            if (FormDetalleProducto == null)
+            if (DatosSourceFinal == 1 || DatosSourceFinal == 3)
             {
-                FormDetalleProducto = new AgregarDetalleProducto();
-                FormDetalleProducto.Show();
-                FormDetalleProducto.BringToFront();
+                //Verifica que el formulario ya tenga una instancia creada, de lo contrario la crea
+                if (FormDetalleProducto == null)
+                {
+                    FormDetalleProducto = new AgregarDetalleProducto();
+                    FormDetalleProducto.typeDatoProveedor = 1;
+                    FormDetalleProducto.Show();
+                    FormDetalleProducto.BringToFront();
+                }
+                else
+                {
+                    FormDetalleProducto.typeDatoProveedor = 1;
+                    FormDetalleProducto.Show();
+                    FormDetalleProducto.BringToFront();
+                }
             }
-            else
+            if (DatosSourceFinal == 2)
             {
-                FormDetalleProducto.Show();
-                FormDetalleProducto.BringToFront();
+                //MessageBox.Show("En Proceso para mostrar Proveedor...", "Seccion en proceso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                int idProductoEditar = Convert.ToInt32(cn.EjecutarSelect($"SELECT ID FROM Productos WHERE ID = '{idEditarProducto}'", 1));
+                
+                int idProveedorConsultar = Convert.ToInt32(cn.EjecutarSelect($"SELECT IDProveedor FROM DetallesProducto WHERE IDProducto = '{idProductoEditar}'", 2));
+
+                //Verifica que el formulario ya tenga una instancia creada, de lo contrario la crea
+                if (FormDetalleProducto == null)
+                {
+                    FormDetalleProducto = new AgregarDetalleProducto();
+                    FormDetalleProducto.typeDatoProveedor = 2;
+                    FormDetalleProducto.IDProveedorAsignado = idProveedorConsultar.ToString();
+                    FormDetalleProducto.Show();
+                    FormDetalleProducto.BringToFront();
+                }
+                else
+                {
+                    FormDetalleProducto.IDProveedorAsignado = idProveedorConsultar.ToString();
+                    FormDetalleProducto.typeDatoProveedor = 2;
+                    FormDetalleProducto.Show();
+                    FormDetalleProducto.BringToFront();
+                }
             }
         }
 
