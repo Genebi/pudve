b3d7bb97253bb4a4b3fdd5888011b86032c20b3e
Genebi <genebi@outlook.com>
2019-09-05 14:00:53 -0500

    prueba

--- PuntoDeVentaV2/Consultas.cs
+++ PuntoDeVentaV2/Consultas.cs
@@ -269,5 +269,7 @@ namespace PuntoDeVentaV2
                    consulta += $"VALUES ('{datos[0]}', '{datos[1]}', '{datos[2]}', '{datos[3]}', '{datos[4]}', '{datos[5]}', '{datos[6]}', '{datos[7]}', '{datos[8]}', '{datos[9]}')";
             return consulta;
         }
+
+        //hola mundo
     }
 }
