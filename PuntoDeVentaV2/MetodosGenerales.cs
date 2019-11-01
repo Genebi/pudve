de5c9d4b78cde2347907b66718ad506aebe81a68
Genebi <genebi@outlook.com>
2019-11-01 16:08:31 -0600

    Nueva columna en Productos y clase MetodosGenerales

---
+++ PuntoDeVentaV2/MetodosGenerales.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuntoDeVentaV2
+{
+    class MetodosGenerales
+    {
+        public string RemoverCaracteres(string nombre)
+        {
+            nombre = nombre.Replace('Á', 'A')
+                           .Replace('É', 'E')
+                           .Replace('Í', 'I')
+                           .Replace('Ó', 'O')
+                           .Replace('Ú', 'U')
+                           .Replace('Ñ', 'N');
+
+            return nombre;
+        }
+    }
+}
