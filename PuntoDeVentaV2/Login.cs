21354fc5c6d1dd91455cace963801c27f5f559be
Emmanuel <emmanuel.ruiz.garcia@outlook.com>
2019-11-28 14:14:11 -0600

    Feat: I did, fourth initialization of the DB configuration
    I did it, fourth initialization of the DB configuration table to the Employees variable in the class constructor
    
    Date: 28/November/2019 02:13 P.M., Feat Made by: Emmanuel Ruiz Garcia

--- PuntoDeVentaV2/Login.cs
+++ PuntoDeVentaV2/Login.cs
@@ -2733,6 +2733,83 @@ namespace PuntoDeVentaV2
                 }
             }
             #endregion TablaCodigoBarrasGenerado
+            // 31 Empleados
+            #region TablaEmpleados
+            tabla = "Empleados";
+            try
+            {
+                checkEmpty(tabla);
+            }
+            catch (Exception ex)
+            {
+                queryTabla = dbTables.QueryNvaTablaEmpleados(tabla);
+                cn.CrearTabla(queryTabla);
+            }
+            if (IsEmpty == true)
+            {
+                try
+                {
+                    count = cn.CountColumnasTabla(dbTables.PragmaTablaEmpleados(tabla));
+                    if (dbTables.GetEmpleados() > count)
+                    {
+                        if (count == 0)
+                        {
+                            queryTabla = dbTables.QueryNvaTablaEmpleados(tabla);
+                            cn.CrearTabla(queryTabla);
+                        }
+                        if (count > 0 && count < dbTables.GetEmpleados())
+                        {
+                            cn.ForeginKeysOff();
+                            queryTabla = dbTables.QueryRenameEmpleados(tabla);
+                            cn.renameTable(queryTabla);
+                            queryTabla = dbTables.QueryNvaTablaEmpleados(tabla);
+                            cn.CrearTabla(queryTabla);
+                            cn.ForeginKeysOn();
+                            queryTabla = dbTables.QueryUpdateTablaEmpleados(tabla);
+                            cn.insertDataIntoTable(queryTabla);
+                            queryTabla = dbTables.DropTablaEmpleados(tabla);
+                            cn.dropOldTable(queryTabla);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al checar la tabla: " + tabla + " error No: " + ex.Message.ToString(), "Error de Checar Tablas", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else if (IsEmpty == false)
+            {
+                try
+                {
+                    count = cn.CountColumnasTabla(dbTables.PragmaTablaEmpleados(tabla));
+                    if (dbTables.GetEmpleados() > count)
+                    {
+                        if (count == 0)
+                        {
+                            queryTabla = dbTables.QueryNvaTablaEmpleados(tabla);
+                            cn.CrearTabla(queryTabla);
+                        }
+                        if (count > 0 && count < dbTables.GetEmpleados())
+                        {
+                            cn.ForeginKeysOff();
+                            queryTabla = dbTables.QueryRenameEmpleados(tabla);
+                            cn.renameTable(queryTabla);
+                            queryTabla = dbTables.QueryNvaTablaEmpleados(tabla);
+                            cn.CrearTabla(queryTabla);
+                            cn.ForeginKeysOn();
+                            queryTabla = dbTables.QueryUpdateTablaEmpleados(tabla);
+                            cn.insertDataIntoTable(queryTabla);
+                            queryTabla = dbTables.DropTablaEmpleados(tabla);
+                            cn.dropOldTable(queryTabla);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al checar la tabla: " + tabla + " error No: " + ex.Message.ToString(), "Error de Checar Tablas", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            #endregion TablaEmpleados
         }
 
         private bool checkEmpty(object tabla)
