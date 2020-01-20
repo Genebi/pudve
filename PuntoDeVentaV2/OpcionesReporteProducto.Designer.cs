e207fd0c8c1948a2cdc00311f739bdba785c6848
Genebi <genebi@outlook.com>
2020-01-20 16:05:24 -0600

    Nuevo form para personalizar reporte de productos

---
+++ PuntoDeVentaV2/OpcionesReporteProducto.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace PuntoDeVentaV2
+{
+    partial class OpcionesReporteProducto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelContenedor = new System.Windows.Forms.Panel();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.primerSeparador = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panelContenedor
+            // 
+            this.panelContenedor.AutoScroll = true;
+            this.panelContenedor.Location = new System.Drawing.Point(2, 7);
+            this.panelContenedor.Name = "panelContenedor";
+            this.panelContenedor.Size = new System.Drawing.Size(379, 301);
+            this.panelContenedor.TabIndex = 0;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.BackColor = System.Drawing.Color.Green;
+            this.btnAceptar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAceptar.FlatAppearance.BorderSize = 0;
+            this.btnAceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAceptar.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAceptar.ForeColor = System.Drawing.Color.White;
+            this.btnAceptar.Location = new System.Drawing.Point(272, 321);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(105, 28);
+            this.btnAceptar.TabIndex = 17;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = false;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancelar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(53)))), ((int)(((byte)(20)))));
+            this.btnCancelar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCancelar.FlatAppearance.BorderSize = 0;
+            this.btnCancelar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(83)))), ((int)(((byte)(79)))));
+            this.btnCancelar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(83)))), ((int)(((byte)(79)))));
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.btnCancelar.ForeColor = System.Drawing.Color.White;
+            this.btnCancelar.Location = new System.Drawing.Point(143, 322);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(105, 27);
+            this.btnCancelar.TabIndex = 18;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // primerSeparador
+            // 
+            this.primerSeparador.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.primerSeparador.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.primerSeparador.Location = new System.Drawing.Point(7, 314);
+            this.primerSeparador.Name = "primerSeparador";
+            this.primerSeparador.Size = new System.Drawing.Size(370, 2);
+            this.primerSeparador.TabIndex = 22;
+            this.primerSeparador.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // OpcionesReporteProducto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.primerSeparador);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.panelContenedor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "OpcionesReporteProducto";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PUDVE - Opciones Reporte";
+            this.Load += new System.EventHandler(this.OpcionesReporteProducto_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelContenedor;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label primerSeparador;
+    }
+}
---
+++ PuntoDeVentaV2/OpcionesReporteProducto.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace PuntoDeVentaV2
+{
+    public partial class OpcionesReporteProducto : Form
+    {
+        private Dictionary<string, string> opcionesDefault;
+        private Dictionary<string, string> seleccionados;
+        private List<string> ultimos;
+        private string ultimoSeleccionado = string.Empty;
+        private int contador = 1;
+
+        public OpcionesReporteProducto()
+        {
+            InitializeComponent();
+        }
+
+        private void OpcionesReporteProducto_Load(object sender, EventArgs e)
+        {
+            ultimos = new List<string>();
+            seleccionados = new Dictionary<string, string>();
+            opcionesDefault = new Dictionary<string, string>();
+
+            opcionesDefault.Add("Nombre", "Nombre producto");
+            opcionesDefault.Add("Precio", "Precio producto");
+            opcionesDefault.Add("PrecioCompra", "Precio de compra");
+            opcionesDefault.Add("PrecioVenta", "Precio de venta");
+            opcionesDefault.Add("Stock", "Stock");
+            opcionesDefault.Add("StockMinimo", "Stock mínimo");
+            opcionesDefault.Add("StockMaximo", "Stock máximo");
+            opcionesDefault.Add("Clave", "Clave de producto");
+            opcionesDefault.Add("Codigo", "Código de barras");
+            opcionesDefault.Add("CodigoExt", "Código de barras extra");
+            opcionesDefault.Add("Proveedor", "Proveedor");
+            opcionesDefault.Add("CantidadPedir", "Cantidad a pedir");
+
+            ObtenerDetalles();
+            VisualizarDetalles();
+        }
+
+        private void ObtenerDetalles()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+
+            if (Properties.Settings.Default.TipoEjecucion == 1)
+            {
+                xmlDoc.Load(Properties.Settings.Default.baseDirectory + Properties.Settings.Default.archivo);
+            }
+
+            if (Properties.Settings.Default.TipoEjecucion == 2)
+            {
+                xmlDoc.Load(Properties.Settings.Default.baseDirectory + Properties.Settings.Default.archivo);
+            }
+
+            // Obtenemos el nodo principal de las propiedades del archivo App.config
+            XmlNode appSettingsNode = xmlDoc.SelectSingleNode("configuration/appSettings");
+
+            //======================================================================
+            //======================================================================
+            foreach (XmlNode childNode in appSettingsNode)
+            {
+                var key = childNode.Attributes["key"].Value;
+                var value = Convert.ToBoolean(childNode.Attributes["value"].Value);
+
+                // Ignoramos los checkbox secundarios de cada propiedad
+                if (key.Substring(0, 3) == "chk")
+                {
+                    continue;
+                }
+
+                // Si el valor de la propiedad es true (esta habilitado)
+                if (value == true)
+                {
+                    // Este valor de proveedor esta agregado por defecto
+                    if (key == "Proveedor")
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        opcionesDefault.Add(key, key);
+                    }
+                }
+            }
+        }
+
+        private void VisualizarDetalles()
+        {
+            if (opcionesDefault.Count > 0)
+            {
+                int alturaEjeY = 10;
+
+                foreach (var opcion in opcionesDefault)
+                {
+                    FlowLayoutPanel panelCustom = new FlowLayoutPanel();
+                    panelCustom.Name = "panel" + opcion.Key;
+                    panelCustom.Width = 350;
+                    panelCustom.Height = 30;
+                    panelCustom.FlowDirection = FlowDirection.LeftToRight;
+                    panelCustom.Location = new Point(3, alturaEjeY);
+                    //panelCustom.BorderStyle = BorderStyle.FixedSingle;
+
+                    CheckBox cbCustom = new CheckBox();
+                    cbCustom.Name = "cb" + opcion.Key;
+                    cbCustom.Tag = opcion.Key;
+                    cbCustom.AutoSize = true;
+                    cbCustom.CheckAlign = ContentAlignment.MiddleLeft;
+                    cbCustom.CheckedChanged += cbCustom_CheckedChanged;
+
+                    Label lbCustom = new Label();
+                    lbCustom.Text = opcion.Value;
+                    lbCustom.Name = "lb" + opcion.Key;
+                    lbCustom.Width = 200;
+                    lbCustom.Height = 20;
+                    lbCustom.TextAlign = ContentAlignment.MiddleLeft;
+
+                    panelCustom.Controls.Add(cbCustom);
+                    panelCustom.Controls.Add(lbCustom);
+                    panelContenedor.Controls.Add(panelCustom);
+
+                    alturaEjeY += 30;
+                }
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            if (seleccionados.Count > 0)
+            {
+                foreach (var opcion in seleccionados)
+                {
+                    //MessageBox.Show(opcion);
+                }
+            }
+        }
+
+        private void cbCustom_CheckedChanged(object sender, EventArgs e)
+        {
+            var checkbox = (CheckBox)sender;
+
+            if (checkbox.Checked)
+            {
+                // Agregamos el nombre de la opcion marcada en el checkbox
+                //seleccionados.Add(checkbox.Name, checkbox.Name);
+
+                foreach (Control panelHijo in panelContenedor.Controls)
+                {
+                    foreach (Control cb in panelHijo.Controls)
+                    {
+                        // Despues de recorrer los paneles verificamos si el control analizado es un checkbox
+                        if (cb is CheckBox)
+                        {
+                            var cbTmp = (CheckBox)cb;
+
+                            // Si el nombre del checkbox seleccionado es diferentea a los checkbox marcados
+                            // previamente los deshabiltamos
+                            if (checkbox.Name != cbTmp.Name && cbTmp.Checked == true)
+                            {
+                                cbTmp.Enabled = false;
+                                ultimoSeleccionado = cbTmp.Name;
+                            }
+                        }
+                    } 
+                }
+
+                // Agregamos el ultimo seleccionado
+                ultimos.Add(ultimoSeleccionado);
+            }
+            else
+            {
+                if (ultimos.Count > 0)
+                {
+                    foreach (var ultimo in ultimos)
+                    {
+                        MessageBox.Show(ultimo);
+                    }
+                }
+            }
+        }
+    }
+}
