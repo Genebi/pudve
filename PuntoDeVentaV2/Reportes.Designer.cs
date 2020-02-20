ea85930831669ee6ade583fab7cfdd3e07502b9f
Genebi <genebi@outlook.com>
2020-02-20 16:10:20 -0600

    Form de Reportes creado

---
+++ PuntoDeVentaV2/Reportes.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace PuntoDeVentaV2
+{
+    partial class Reportes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tituloSeccion = new System.Windows.Forms.Label();
+            this.primerSeparador = new System.Windows.Forms.Label();
+            this.btnHistorialPrecios = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tituloSeccion
+            // 
+            this.tituloSeccion.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.tituloSeccion.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tituloSeccion.Location = new System.Drawing.Point(42, 19);
+            this.tituloSeccion.Name = "tituloSeccion";
+            this.tituloSeccion.Size = new System.Drawing.Size(900, 25);
+            this.tituloSeccion.TabIndex = 24;
+            this.tituloSeccion.Text = "REPORTES";
+            this.tituloSeccion.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // primerSeparador
+            // 
+            this.primerSeparador.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.primerSeparador.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.primerSeparador.Location = new System.Drawing.Point(42, 61);
+            this.primerSeparador.Name = "primerSeparador";
+            this.primerSeparador.Size = new System.Drawing.Size(900, 2);
+            this.primerSeparador.TabIndex = 23;
+            this.primerSeparador.Text = "REPORTES";
+            this.primerSeparador.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnHistorialPrecios
+            // 
+            this.btnHistorialPrecios.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(53)))), ((int)(((byte)(20)))));
+            this.btnHistorialPrecios.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnHistorialPrecios.FlatAppearance.BorderSize = 0;
+            this.btnHistorialPrecios.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(83)))), ((int)(((byte)(79)))));
+            this.btnHistorialPrecios.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(83)))), ((int)(((byte)(79)))));
+            this.btnHistorialPrecios.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHistorialPrecios.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHistorialPrecios.ForeColor = System.Drawing.Color.White;
+            this.btnHistorialPrecios.Location = new System.Drawing.Point(42, 91);
+            this.btnHistorialPrecios.Name = "btnHistorialPrecios";
+            this.btnHistorialPrecios.Size = new System.Drawing.Size(190, 30);
+            this.btnHistorialPrecios.TabIndex = 102;
+            this.btnHistorialPrecios.Text = "Historial de Precios";
+            this.btnHistorialPrecios.UseVisualStyleBackColor = false;
+            this.btnHistorialPrecios.Click += new System.EventHandler(this.btnHistorialPrecios_Click);
+            // 
+            // Reportes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 661);
+            this.Controls.Add(this.btnHistorialPrecios);
+            this.Controls.Add(this.tituloSeccion);
+            this.Controls.Add(this.primerSeparador);
+            this.Name = "Reportes";
+            this.Text = "Reportes";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label tituloSeccion;
+        private System.Windows.Forms.Label primerSeparador;
+        private System.Windows.Forms.Button btnHistorialPrecios;
+    }
+}
