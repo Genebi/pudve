95bebc0dd5ad4b9454a1ba3d1045276a4f2b7a80
Genebi <genebi@outlook.com>
2020-04-10 12:38:25 -0500

    Simplificando codigo de las opciones estaticas de boton asignar

--- PuntoDeVentaV2/AsignarMultipleProductos.cs
+++ PuntoDeVentaV2/AsignarMultipleProductos.cs
@@ -31,15 +31,42 @@ namespace PuntoDeVentaV2
 
             var propiedad = btn.Tag;
 
-            AsignarPropiedad ap = new AsignarPropiedad(propiedad);
-
-            ap.FormClosed += delegate
+            using (var ap = new AsignarPropiedad(propiedad))
             {
-
-            };
-
-            ap.ShowDialog();
+                ap.ShowDialog();
+            }
         }
+
+        private void AgregarOpcion(string nombre, string texto, int altura)
+        {
+            // PANEL MENSAJE VENTAS
+            FlowLayoutPanel panelCustom = new FlowLayoutPanel();
+            panelCustom.Name = "panel" + nombre;
+            panelCustom.Width = 350;
+            panelCustom.Height = 30;
+            panelCustom.FlowDirection = FlowDirection.LeftToRight;
+            panelCustom.Location = new Point(3, altura);
+            panelCustom.BorderStyle = BorderStyle.FixedSingle;
+
+            Label lbCustom = new Label();
+            lbCustom.Text = texto;
+            lbCustom.Name = "lb" + nombre;
+            lbCustom.Width = 100;
+            lbCustom.Height = 20;
+            lbCustom.TextAlign = ContentAlignment.MiddleCenter;
+
+            Button btnCustom = new Button();
+            btnCustom.Name = "btn" + nombre;
+            btnCustom.Text = "Asignar";
+            btnCustom.Cursor = Cursors.Hand;
+            btnCustom.Tag = nombre;
+            btnCustom.Click += new EventHandler(botonAsignar_Click);
+
+            panelCustom.Controls.Add(lbCustom);
+            panelCustom.Controls.Add(btnCustom);
+            panelContenedor.Controls.Add(panelCustom);
+        }
+
 
         private void CargarPropiedades()
         {
@@ -61,274 +88,34 @@ namespace PuntoDeVentaV2
             //======================================================================
 
             // PANEL MENSAJE VENTAS
-            FlowLayoutPanel panelMensaje = new FlowLayoutPanel();
-            panelMensaje.Name = "panelMensaje";
-            panelMensaje.Width = 350;
-            panelMensaje.Height = 30;
-            panelMensaje.FlowDirection = FlowDirection.LeftToRight;
-            panelMensaje.Location = new Point(3, 10);
-            panelMensaje.BorderStyle = BorderStyle.FixedSingle;
-
-            Label lbMensaje = new Label();
-            lbMensaje.Text = "Mensaje Ventas";
-            lbMensaje.Name = "lbMensaje";
-            lbMensaje.Width = 100;
-            lbMensaje.Height = 20;
-            lbMensaje.TextAlign = ContentAlignment.MiddleCenter;
-
-            Button btnMensaje = new Button();
-            btnMensaje.Name = "btnMensaje";
-            btnMensaje.Text = "Asignar";
-            btnMensaje.Cursor = Cursors.Hand;
-            btnMensaje.Tag = "Mensaje";
-            btnMensaje.Click += new EventHandler(botonAsignar_Click);
-
-            panelMensaje.Controls.Add(lbMensaje);
-            panelMensaje.Controls.Add(btnMensaje);
-            panelContenedor.Controls.Add(panelMensaje);
+            AgregarOpcion("Mensaje", "Mensaje Ventas", 10);
 
             // PANEL MENSAJE INVENTARIO
-            FlowLayoutPanel panelMensajeInventario = new FlowLayoutPanel();
-            panelMensajeInventario.Name = "panelMensajeInventario";
-            panelMensajeInventario.Width = 350;
-            panelMensajeInventario.Height = 30;
-            panelMensajeInventario.FlowDirection = FlowDirection.LeftToRight;
-            panelMensajeInventario.Location = new Point(3, 45);
-            panelMensajeInventario.BorderStyle = BorderStyle.FixedSingle;
-
-            Label lbMensajeInventario = new Label();
-            lbMensajeInventario.Text = "Mensaje Inventario";
-            lbMensajeInventario.Name = "lbMensajeInventario";
-            lbMensajeInventario.Width = 100;
-            lbMensajeInventario.Height = 20;
-            lbMensajeInventario.TextAlign = ContentAlignment.MiddleCenter;
-
-            Button btnMensajeInventario = new Button();
-            btnMensajeInventario.Name = "btnMensajeInventario";
-            btnMensajeInventario.Text = "Asignar";
-            btnMensajeInventario.Cursor = Cursors.Hand;
-            btnMensajeInventario.Tag = "MensajeInventario";
-            btnMensajeInventario.Click += new EventHandler(botonAsignar_Click);
-
-            panelMensajeInventario.Controls.Add(lbMensajeInventario);
-            panelMensajeInventario.Controls.Add(btnMensajeInventario);
-            panelContenedor.Controls.Add(panelMensajeInventario);
+            AgregarOpcion("MensajeInventario", "Mensaje Inventario", 45);
 
             // PANEL STOCK
-            FlowLayoutPanel panelStock = new FlowLayoutPanel();
-            panelStock.Name = "panelStock";
-            panelStock.Width = 350;
-            panelStock.Height = 30;
-            panelStock.FlowDirection = FlowDirection.LeftToRight;
-            panelStock.Location = new Point(3, 80);
-            panelStock.BorderStyle = BorderStyle.FixedSingle;
-
-            Label lbStock = new Label();
-            lbStock.Text = "Stock";
-            lbStock.Name = "lbStock";
-            lbStock.Width = 100;
-            lbStock.Height = 20;
-            lbStock.TextAlign = ContentAlignment.MiddleCenter;
-
-            Button btnStock = new Button();
-            btnStock.Name = "btnStock";
-            btnStock.Text = "Asignar";
-            btnStock.Cursor = Cursors.Hand;
-            btnStock.Tag = "Stock";
-            btnStock.Click += new EventHandler(botonAsignar_Click);
-
-            panelStock.Controls.Add(lbStock);
-            panelStock.Controls.Add(btnStock);
-            panelContenedor.Controls.Add(panelStock);
+            AgregarOpcion("Stock", "Stock", 80);
 
             // PANEL STOCK MINIMO
-            FlowLayoutPanel panelStockMinimo = new FlowLayoutPanel();
-            panelStockMinimo.Name = "panelStockMinimo";
-            panelStockMinimo.Width = 350;
-            panelStockMinimo.Height = 30;
-            panelStockMinimo.FlowDirection = FlowDirection.LeftToRight;
-            panelStockMinimo.Location = new Point(3, 115);
-            panelStockMinimo.BorderStyle = BorderStyle.FixedSingle;
-
-            Label lbStockMinimo = new Label();
-            lbStockMinimo.Text = "Stock Minimo";
-            lbStockMinimo.Name = "lbStockMinimo";
-            lbStockMinimo.Width = 100;
-            lbStockMinimo.Height = 20;
-            lbStockMinimo.TextAlign = ContentAlignment.MiddleCenter;
-
-            Button btnStockMinimo = new Button();
-            btnStockMinimo.Name = "btnStockMinimo";
-            btnStockMinimo.Text = "Asignar";
-            btnStockMinimo.Cursor = Cursors.Hand;
-            btnStockMinimo.Tag = "StockMinimo";
-            btnStockMinimo.Click += new EventHandler(botonAsignar_Click);
-
-            panelStockMinimo.Controls.Add(lbStockMinimo);
-            panelStockMinimo.Controls.Add(btnStockMinimo);
-            panelContenedor.Controls.Add(panelStockMinimo);
+            AgregarOpcion("StockMinimo", "Stock Minimo", 115);
 
             // PANEL STOCK MAXIMO
-            FlowLayoutPanel panelStockMaximo = new FlowLayoutPanel();
-            panelStockMaximo.Name = "panelStockMaximo";
-            panelStockMaximo.Width = 350;
-            panelStockMaximo.Height = 30;
-            panelStockMaximo.FlowDirection = FlowDirection.LeftToRight;
-            panelStockMaximo.Location = new Point(3, 150);
-            panelStockMaximo.BorderStyle = BorderStyle.FixedSingle;
-
-            Label lbStockMaximo = new Label();
-            lbStockMaximo.Text = "Stock Maximo";
-            lbStockMaximo.Name = "lbStockMaximo";
-            lbStockMaximo.Width = 100;
-            lbStockMaximo.Height = 20;
-            lbStockMaximo.TextAlign = ContentAlignment.MiddleCenter;
-
-            Button btnStockMaximo = new Button();
-            btnStockMaximo.Name = "btnStockMaximo";
-            btnStockMaximo.Text = "Asignar";
-            btnStockMaximo.Cursor = Cursors.Hand;
-            btnStockMaximo.Tag = "StockMaximo";
-            btnStockMaximo.Click += new EventHandler(botonAsignar_Click);
-
-            panelStockMaximo.Controls.Add(lbStockMaximo);
-            panelStockMaximo.Controls.Add(btnStockMaximo);
-            panelContenedor.Controls.Add(panelStockMaximo);
+            AgregarOpcion("StockMaximo", "Stock Maximo", 150);
 
             // PANEL PRECIO
-            FlowLayoutPanel panelPrecio = new FlowLayoutPanel();
-            panelPrecio.Name = "panelPrecio";
-            panelPrecio.Width = 350;
-            panelPrecio.Height = 30;
-            panelPrecio.FlowDirection = FlowDirection.LeftToRight;
-            panelPrecio.Location = new Point(3, 185);
-            panelPrecio.BorderStyle = BorderStyle.FixedSingle;
-
-            Label lbPrecio = new Label();
-            lbPrecio.Text = "Precio";
-            lbPrecio.Name = "lbPrecio";
-            lbPrecio.Width = 100;
-            lbPrecio.Height = 20;
-            lbPrecio.TextAlign = ContentAlignment.MiddleCenter;
-
-            Button btnPrecio = new Button();
-            btnPrecio.Name = "btnPrecio";
-            btnPrecio.Text = "Asignar";
-            btnPrecio.Cursor = Cursors.Hand;
-            btnPrecio.Tag = "Precio";
-            btnPrecio.Click += new EventHandler(botonAsignar_Click);
-
-            panelPrecio.Controls.Add(lbPrecio);
-            panelPrecio.Controls.Add(btnPrecio);
-            panelContenedor.Controls.Add(panelPrecio);
+            AgregarOpcion("Precio", "Precio", 185);
 
             // PANEL NUMERO DE REVISION
-            FlowLayoutPanel panelRevision = new FlowLayoutPanel();
-            panelRevision.Name = "panelRevision";
-            panelRevision.Width = 350;
-            panelRevision.Height = 30;
-            panelRevision.FlowDirection = FlowDirection.LeftToRight;
-            panelRevision.Location = new Point(3, 220);
-            panelRevision.BorderStyle = BorderStyle.FixedSingle;
-
-            Label lbRevision = new Label();
-            lbRevision.Text = "Número Revisión";
-            lbRevision.Name = "lbRevision";
-            lbRevision.Width = 100;
-            lbRevision.Height = 20;
-            lbRevision.TextAlign = ContentAlignment.MiddleCenter;
-
-            Button btnRevision = new Button();
-            btnRevision.Name = "btnRevision";
-            btnRevision.Text = "Asignar";
-            btnRevision.Cursor = Cursors.Hand;
-            btnRevision.Tag = "NumeroRevision";
-            btnRevision.Click += new EventHandler(botonAsignar_Click);
-
-            panelRevision.Controls.Add(lbRevision);
-            panelRevision.Controls.Add(btnRevision);
-            panelContenedor.Controls.Add(panelRevision);
+            AgregarOpcion("Revision", "Número Revisión", 220);
 
             // PANEL TIPO DE IVA
-            FlowLayoutPanel panelIVA = new FlowLayoutPanel();
-            panelIVA.Name = "panelIVA";
-            panelIVA.Width = 350;
-            panelIVA.Height = 30;
-            panelIVA.FlowDirection = FlowDirection.LeftToRight;
-            panelIVA.Location = new Point(3, 255);
-            panelIVA.BorderStyle = BorderStyle.FixedSingle;
-
-            Label lbIVA = new Label();
-            lbIVA.Text = "Tipo de IVA";
-            lbIVA.Name = "lbIVA";
-            lbIVA.Width = 100;
-            lbIVA.Height = 20;
-            lbIVA.TextAlign = ContentAlignment.MiddleCenter;
-
-            Button btnIVA = new Button();
-            btnIVA.Name = "btnIVA";
-            btnIVA.Text = "Asignar";
-            btnIVA.Cursor = Cursors.Hand;
-            btnIVA.Tag = "TipoIVA";
-            btnIVA.Click += new EventHandler(botonAsignar_Click);
-
-            panelIVA.Controls.Add(lbIVA);
-            panelIVA.Controls.Add(btnIVA);
-            panelContenedor.Controls.Add(panelIVA);
+            AgregarOpcion("IVA", "Tipo de IVA", 255);
 
             // PANEL CLAVE DE PRODUCTO (FACTURACION)
-            FlowLayoutPanel panelClaveProducto = new FlowLayoutPanel();
-            panelClaveProducto.Name = "panelClaveProducto";
-            panelClaveProducto.Width = 350;
-            panelClaveProducto.Height = 30;
-            panelClaveProducto.FlowDirection = FlowDirection.LeftToRight;
-            panelClaveProducto.Location = new Point(3, 290);
-            panelClaveProducto.BorderStyle = BorderStyle.FixedSingle;
-
-            Label lbClaveProducto = new Label();
-            lbClaveProducto.Text = "Clave de Producto";
-            lbClaveProducto.Name = "lbClaveProducto";
-            lbClaveProducto.Width = 100;
-            lbClaveProducto.Height = 20;
-            lbClaveProducto.TextAlign = ContentAlignment.MiddleCenter;
-
-            Button btnClaveProducto = new Button();
-            btnClaveProducto.Name = "btnClaveProducto";
-            btnClaveProducto.Text = "Asignar";
-            btnClaveProducto.Cursor = Cursors.Hand;
-            btnClaveProducto.Tag = "ClaveProducto";
-            btnClaveProducto.Click += new EventHandler(botonAsignar_Click);
-
-            panelClaveProducto.Controls.Add(lbClaveProducto);
-            panelClaveProducto.Controls.Add(btnClaveProducto);
-            panelContenedor.Controls.Add(panelClaveProducto);
+            AgregarOpcion("ClaveProducto", "Clave de Producto", 290);
 
             // PANEL CLAVE UNIDAD MEDIDA (FACTURACION)
-            FlowLayoutPanel panelClaveUnidad = new FlowLayoutPanel();
-            panelClaveUnidad.Name = "panelClaveUnidad";
-            panelClaveUnidad.Width = 350;
-            panelClaveUnidad.Height = 30;
-            panelClaveUnidad.FlowDirection = FlowDirection.LeftToRight;
-            panelClaveUnidad.Location = new Point(3, 325);
-            panelClaveUnidad.BorderStyle = BorderStyle.FixedSingle;
-
-            Label lbClaveUnidad = new Label();
-            lbClaveUnidad.Text = "Clave de Unidad";
-            lbClaveUnidad.Name = "lbClaveUnidad";
-            lbClaveUnidad.Width = 100;
-            lbClaveUnidad.Height = 20;
-            lbClaveUnidad.TextAlign = ContentAlignment.MiddleCenter;
-
-            Button btnClaveUnidad = new Button();
-            btnClaveUnidad.Name = "btnClaveUnidad";
-            btnClaveUnidad.Text = "Asignar";
-            btnClaveUnidad.Cursor = Cursors.Hand;
-            btnClaveUnidad.Tag = "ClaveUnidad";
-            btnClaveUnidad.Click += new EventHandler(botonAsignar_Click);
-
-            panelClaveUnidad.Controls.Add(lbClaveUnidad);
-            panelClaveUnidad.Controls.Add(btnClaveUnidad);
-            panelContenedor.Controls.Add(panelClaveUnidad);
+            AgregarOpcion("ClaveUnidad", "Clave de Unidad", 325);
 
             int alturaEjeY = 360;
 
