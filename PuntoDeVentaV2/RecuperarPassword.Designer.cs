8c393e11e95e608633680f706bfe2bca25ea0e0c
Genebi <genebi@outlook.com>
2020-04-25 10:19:42 -0500

    Form para recuperar password I

---
+++ PuntoDeVentaV2/RecuperarPassword.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace PuntoDeVentaV2
+{
+    partial class RecuperarPassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.btnRecuperar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtUsuario.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.Location = new System.Drawing.Point(70, 68);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(214, 23);
+            this.txtUsuario.TabIndex = 0;
+            this.txtUsuario.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtUsuario.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtUsuario_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(104, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Introduzca el usuario";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.ForeColor = System.Drawing.Color.Red;
+            this.label14.Location = new System.Drawing.Point(91, 35);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(166, 17);
+            this.label14.TabIndex = 22;
+            this.label14.Text = "[Ejemplo: ABARROTES2020]";
+            // 
+            // btnRecuperar
+            // 
+            this.btnRecuperar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRecuperar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(104)))), ((int)(((byte)(144)))));
+            this.btnRecuperar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRecuperar.FlatAppearance.BorderSize = 0;
+            this.btnRecuperar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(104)))), ((int)(((byte)(190)))));
+            this.btnRecuperar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(104)))), ((int)(((byte)(190)))));
+            this.btnRecuperar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRecuperar.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRecuperar.ForeColor = System.Drawing.Color.White;
+            this.btnRecuperar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRecuperar.Location = new System.Drawing.Point(70, 99);
+            this.btnRecuperar.Name = "btnRecuperar";
+            this.btnRecuperar.Size = new System.Drawing.Size(214, 27);
+            this.btnRecuperar.TabIndex = 23;
+            this.btnRecuperar.Text = "Recuperar";
+            this.btnRecuperar.UseVisualStyleBackColor = false;
+            this.btnRecuperar.Click += new System.EventHandler(this.btnRecuperar_Click);
+            // 
+            // RecuperarPassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 151);
+            this.Controls.Add(this.btnRecuperar);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUsuario);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "RecuperarPassword";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Button btnRecuperar;
+    }
+}
---
+++ PuntoDeVentaV2/RecuperarPassword.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PuntoDeVentaV2
+{
+    public partial class RecuperarPassword : Form
+    {
+        MetodosBusquedas mb = new MetodosBusquedas();
+
+        public RecuperarPassword()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRecuperar_Click(object sender, EventArgs e)
+        {
+            var usuario = txtUsuario.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(usuario))
+            {
+                MessageBox.Show("El nick de usuario es obligatorio", "Mensaje del sistema", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                return;
+            }
+
+            var datos = mb.RecuperarPassword(usuario);
+
+            if (datos.Length > 0)
+            {
+                var email = datos[1];
+
+                if (!string.IsNullOrWhiteSpace(email))
+                {
+                    var razon = datos[0];
+                    var password = datos[2];
+                    
+                    var html = $@"";
+
+                    //Utilidades.EnviarEmail();
+                }
+            }
+        }
+
+        private void txtUsuario_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                btnRecuperar.PerformClick();
+            }
+        }
+    }
+}
