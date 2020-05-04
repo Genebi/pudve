75c6455d0d5fa4736ef33dec12d8aad1b350b197
Genebi <genebi@outlook.com>
2020-05-04 14:17:52 -0500

    Cambio en el label de clave de producto en detalle de facturacion

--- PuntoDeVentaV2/AgregarDetalleFacturacionProducto.Designer.cs
+++ PuntoDeVentaV2/AgregarDetalleFacturacionProducto.Designer.cs
@@ -202,9 +202,9 @@
             this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(57, 426);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(137, 17);
+            this.label1.Size = new System.Drawing.Size(164, 17);
             this.label1.TabIndex = 35;
-            this.label1.Text = "Clave de producto:";
+            this.label1.Text = "Clave SAT del producto:";
             // 
             // btnClaveGenerica
             // 
@@ -227,7 +227,7 @@
             this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(61, 473);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(133, 40);
+            this.label2.Size = new System.Drawing.Size(160, 40);
             this.label2.TabIndex = 38;
             this.label2.Text = "Clave de unidad de medida:";
             // 
