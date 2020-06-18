e13fd591a61f6936763734645889ccc085ab7f88
Emmanuel <emmanuel.ruiz.garcia@outlook.com>
2020-06-18 10:05:38 -0500

    1.- Agregar Imagen Faltante En Apartado Facturacion (blanco.png) 2.- Para corregir error de excepción de imagen no encotrada

--- PuntoDeVentaV2/Properties/Resources.Designer.cs
+++ PuntoDeVentaV2/Properties/Resources.Designer.cs
@@ -1026,6 +1026,16 @@ namespace PuntoDeVentaV2.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap blanco {
+            get {
+                object obj = ResourceManager.GetObject("blanco", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap blind {
             get {
                 object obj = ResourceManager.GetObject("blind", resourceCulture);
