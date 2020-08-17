02aae87f0ca6c97da6f108370443535f43b85c0f
Emmanuel <emmanuel.ruiz.garcia@outlook.com>
2020-08-17 11:00:11 -0500

    Agregar botón para borrado de Imagen en Lista de Productos

--- PuntoDeVentaV2/photoShow.Designer.cs
+++ PuntoDeVentaV2/photoShow.Designer.cs
@@ -30,34 +30,54 @@
         {
             this.pictureBoxProducto = new System.Windows.Forms.PictureBox();
             this.lblNombreProducto = new System.Windows.Forms.Label();
+            this.btnImagen = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProducto)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBoxProducto
             // 
-            this.pictureBoxProducto.Location = new System.Drawing.Point(26, 72);
+            this.pictureBoxProducto.Location = new System.Drawing.Point(20, 53);
+            this.pictureBoxProducto.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBoxProducto.Name = "pictureBoxProducto";
-            this.pictureBoxProducto.Size = new System.Drawing.Size(425, 331);
+            this.pictureBoxProducto.Size = new System.Drawing.Size(319, 269);
             this.pictureBoxProducto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxProducto.TabIndex = 0;
             this.pictureBoxProducto.TabStop = false;
             // 
             // lblNombreProducto
             // 
-            this.lblNombreProducto.Location = new System.Drawing.Point(23, 22);
+            this.lblNombreProducto.Location = new System.Drawing.Point(20, 17);
+            this.lblNombreProducto.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblNombreProducto.Name = "lblNombreProducto";
-            this.lblNombreProducto.Size = new System.Drawing.Size(428, 23);
+            this.lblNombreProducto.Size = new System.Drawing.Size(319, 19);
             this.lblNombreProducto.TabIndex = 1;
             this.lblNombreProducto.Text = "label1";
             this.lblNombreProducto.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // btnImagen
+            // 
+            this.btnImagen.BackColor = System.Drawing.Color.Green;
+            this.btnImagen.FlatAppearance.BorderSize = 0;
+            this.btnImagen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnImagen.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImagen.ForeColor = System.Drawing.Color.White;
+            this.btnImagen.Location = new System.Drawing.Point(110, 331);
+            this.btnImagen.Name = "btnImagen";
+            this.btnImagen.Size = new System.Drawing.Size(139, 41);
+            this.btnImagen.TabIndex = 2;
+            this.btnImagen.Text = "Borrar imagen";
+            this.btnImagen.UseVisualStyleBackColor = false;
+            this.btnImagen.Click += new System.EventHandler(this.btnImagen_Click);
+            // 
             // photoShow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(481, 468);
+            this.ClientSize = new System.Drawing.Size(361, 380);
+            this.Controls.Add(this.btnImagen);
             this.Controls.Add(this.lblNombreProducto);
             this.Controls.Add(this.pictureBoxProducto);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "photoShow";
@@ -74,5 +94,6 @@
 
         private System.Windows.Forms.PictureBox pictureBoxProducto;
         private System.Windows.Forms.Label lblNombreProducto;
+        private System.Windows.Forms.Button btnImagen;
     }
 }
--- PuntoDeVentaV2/photoShow.cs
+++ PuntoDeVentaV2/photoShow.cs
@@ -81,5 +81,10 @@ namespace PuntoDeVentaV2
                 }
             }
         }
+
+        private void btnImagen_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
