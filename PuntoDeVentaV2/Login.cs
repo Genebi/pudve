d3c9bbd91c2d6acef5732e9f4edaed6771f492c0
Emmanuel <emmanuel.ruiz.garcia@outlook.com>
2020-09-17 10:57:46 -0500

    Verificacion de copiado de Diccionario de Datos para actucalizacion de base de datos

--- PuntoDeVentaV2/Login.cs
+++ PuntoDeVentaV2/Login.cs
@@ -419,9 +419,16 @@ namespace PuntoDeVentaV2
                 // Loop through all files to copy.
                 for (int x = 1; x <= filesToCopy.Length; x++)
                 {
-                    if (!File.Exists(pathDestino + filesToCopy[x - 1].ToString()))
+                    if (filesToCopy[x - 1].ToString().Equals("DataDictionary.db"))
                     {
                         File.Copy(pathOrigen + filesToCopy[x - 1].ToString(), pathDestino + filesToCopy[x - 1].ToString(), true);
+                    }
+                    else
+                    {
+                        if (!File.Exists(pathDestino + filesToCopy[x - 1].ToString()))
+                        {
+                            File.Copy(pathOrigen + filesToCopy[x - 1].ToString(), pathDestino + filesToCopy[x - 1].ToString(), true);
+                        }
                     }
                 }
             }
