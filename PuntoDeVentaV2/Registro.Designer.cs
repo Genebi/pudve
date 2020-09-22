8d8e765e2fa1f8a9efac10671ae40800c162bd07
kevinlg1014@gmail.com <kevinlg1014@gmail.com>
2020-09-22 15:38:34 -0500

    Cambio de diseño en el Registro

--- PuntoDeVentaV2/Registro.Designer.cs
+++ PuntoDeVentaV2/Registro.Designer.cs
@@ -58,9 +58,9 @@
             this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(117, 65);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 17);
+            this.label1.Size = new System.Drawing.Size(156, 17);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Usuario";
+            this.label1.Text = "Cree un Nuevo Usuario";
             // 
             // label2
             // 
@@ -68,9 +68,9 @@
             this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(117, 280);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 17);
+            this.label2.Size = new System.Drawing.Size(195, 17);
             this.label2.TabIndex = 4;
-            this.label2.Text = "E-mail";
+            this.label2.Text = "Ingrese su Correo Electronico";
             // 
             // label3
             // 
@@ -78,9 +78,9 @@
             this.label3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(117, 228);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(159, 17);
+            this.label3.Size = new System.Drawing.Size(131, 17);
             this.label3.TabIndex = 3;
-            this.label3.Text = "Nombre o Razón social";
+            this.label3.Text = "Ingrese su Nombre ";
             // 
             // label4
             // 
@@ -88,9 +88,9 @@
             this.label4.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(117, 119);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(69, 17);
+            this.label4.Size = new System.Drawing.Size(149, 17);
             this.label4.TabIndex = 1;
-            this.label4.Text = "Password";
+            this.label4.Text = "Crea una Contraseña";
             // 
             // label5
             // 
@@ -98,9 +98,9 @@
             this.label5.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(117, 330);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(62, 17);
+            this.label5.Size = new System.Drawing.Size(205, 17);
             this.label5.TabIndex = 5;
-            this.label5.Text = "Teléfono";
+            this.label5.Text = "Ingrese su Número de Teléfono";
             // 
             // txtUsuario
             // 
@@ -165,9 +165,9 @@
             this.label6.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(117, 176);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(138, 17);
+            this.label6.Size = new System.Drawing.Size(210, 17);
             this.label6.TabIndex = 2;
-            this.label6.Text = "Confirmar Password";
+            this.label6.Text = "Vuelva a Escribir su Contraseña";
             // 
             // label7
             // 
@@ -204,7 +204,7 @@
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.Color.Red;
-            this.label8.Location = new System.Drawing.Point(166, 68);
+            this.label8.Location = new System.Drawing.Point(272, 68);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(14, 17);
             this.label8.TabIndex = 14;
@@ -215,7 +215,7 @@
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.ForeColor = System.Drawing.Color.Red;
-            this.label9.Location = new System.Drawing.Point(181, 122);
+            this.label9.Location = new System.Drawing.Point(265, 119);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(14, 17);
             this.label9.TabIndex = 15;
@@ -226,7 +226,7 @@
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label10.ForeColor = System.Drawing.Color.Red;
-            this.label10.Location = new System.Drawing.Point(250, 177);
+            this.label10.Location = new System.Drawing.Point(325, 177);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(14, 17);
             this.label10.TabIndex = 16;
@@ -237,7 +237,7 @@
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label11.ForeColor = System.Drawing.Color.Red;
-            this.label11.Location = new System.Drawing.Point(272, 228);
+            this.label11.Location = new System.Drawing.Point(244, 228);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(14, 17);
             this.label11.TabIndex = 17;
@@ -248,7 +248,7 @@
             this.label12.AutoSize = true;
             this.label12.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label12.ForeColor = System.Drawing.Color.Red;
-            this.label12.Location = new System.Drawing.Point(160, 281);
+            this.label12.Location = new System.Drawing.Point(312, 281);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(14, 17);
             this.label12.TabIndex = 18;
@@ -259,7 +259,7 @@
             this.label13.AutoSize = true;
             this.label13.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label13.ForeColor = System.Drawing.Color.Red;
-            this.label13.Location = new System.Drawing.Point(174, 332);
+            this.label13.Location = new System.Drawing.Point(319, 332);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(14, 17);
             this.label13.TabIndex = 19;
@@ -293,11 +293,12 @@
             this.label14.AutoSize = true;
             this.label14.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label14.ForeColor = System.Drawing.Color.Red;
-            this.label14.Location = new System.Drawing.Point(177, 65);
+            this.label14.Location = new System.Drawing.Point(280, 39);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(166, 17);
             this.label14.TabIndex = 21;
             this.label14.Text = "[Ejemplo: ABARROTES2020]";
+            this.label14.Visible = false;
             // 
             // Registro
             // 
