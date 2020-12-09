da80b06cff11aab2b3daaf1c1762d37ec50c4c02
Emmanuel <emmanuel.ruiz.garcia@outlook.com>
2020-12-09 15:27:38 -0600

    Rectificacion de Envio de correo #1

--- PuntoDeVentaV2/Utilidades.cs
+++ PuntoDeVentaV2/Utilidades.cs
@@ -1386,25 +1386,10 @@ namespace PuntoDeVentaV2
 
             asunto = "Venta no realizada en el sistema";
             correo = datosUsuario[9].ToString();
-            correo1 = "micorreoeshouse_1@hotmail.com";
-            //correo = "genebi@outlook.com";
 
             if (!correo.Equals(""))
             {
                 EnviarEmail(correoHTML, asunto, correo);
-                //if (correo.Equals(correo1))
-                //{
-                //    EnviarEmail(correoHTML, asunto, correo);
-                //}
-                //else
-                //{
-                //    EnviarEmail(correoHTML, asunto, correo);
-                //    EnviarEmail(correoHTML, asunto, correo1);
-                //}
-            }
-            else
-            {
-                //EnviarEmail(correoHTML, asunto, correo1);
             }
         }
     }
