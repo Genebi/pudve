61338205d8182a0f4034db0b368f555b65edac43
Emmanuel <emmanuel.ruiz.garcia@outlook.com>
2020-12-22 15:05:51 -0600

    se cambio foma de llamar calculadora con el espacio en campo Tarjeta en la Forma AgregarRetirarDinero

--- PuntoDeVentaV2/AgregarRetirarDinero.cs
+++ PuntoDeVentaV2/AgregarRetirarDinero.cs
@@ -770,7 +770,7 @@ namespace PuntoDeVentaV2
         private void txtTarjeta_KeyPress(object sender, KeyPressEventArgs e)
         {
             int calcu = 0;
-            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            if (e.KeyChar == Convert.ToChar(Keys.Space))
             {
                 calcu++;
 
