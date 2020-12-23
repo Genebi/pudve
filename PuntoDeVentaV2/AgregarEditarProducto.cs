d7c84229a606938760d1779b9269485ef972dba9
Emmanuel <emmanuel.ruiz.garcia@outlook.com>
2020-12-23 09:54:25 -0600

    llamar calculadora en campo TxtPrecioMayoreo AgregarEditarProducto

--- PuntoDeVentaV2/AgregarEditarProducto.cs
+++ PuntoDeVentaV2/AgregarEditarProducto.cs
@@ -1289,7 +1289,7 @@ namespace PuntoDeVentaV2
                 e.Handled = true;
             }
 
-            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            if (e.KeyChar == Convert.ToChar(Keys.Space))
             {
                 calcu++;
 
