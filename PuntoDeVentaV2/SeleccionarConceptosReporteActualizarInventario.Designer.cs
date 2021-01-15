6dcd6d898ac16378d2e568a25be3c8c91b5b889a
Emmanuel <emmanuel.ruiz.garcia@outlook.com>
2021-01-15 16:01:10 -0600

    se agrego funcionalidad del CheckListedBox

--- PuntoDeVentaV2/SeleccionarConceptosReporteActualizarInventario.Designer.cs
+++ PuntoDeVentaV2/SeleccionarConceptosReporteActualizarInventario.Designer.cs
@@ -28,13 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.CLBConceptosExistentes = new System.Windows.Forms.CheckedListBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.SuspendLayout();
+            // 
+            // CLBConceptosExistentes
+            // 
+            this.CLBConceptosExistentes.CheckOnClick = true;
+            this.CLBConceptosExistentes.FormattingEnabled = true;
+            this.CLBConceptosExistentes.Location = new System.Drawing.Point(12, 117);
+            this.CLBConceptosExistentes.Name = "CLBConceptosExistentes";
+            this.CLBConceptosExistentes.Size = new System.Drawing.Size(200, 154);
+            this.CLBConceptosExistentes.TabIndex = 0;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnAceptar.FlatAppearance.BorderSize = 0;
+            this.btnAceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAceptar.Font = new System.Drawing.Font("Century", 9.75F);
+            this.btnAceptar.ForeColor = System.Drawing.Color.White;
+            this.btnAceptar.Location = new System.Drawing.Point(137, 278);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 1;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = false;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnCancelar.FlatAppearance.BorderSize = 0;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("Century", 9.75F);
+            this.btnCancelar.ForeColor = System.Drawing.Color.White;
+            this.btnCancelar.Location = new System.Drawing.Point(56, 278);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 2;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 35);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Selecciona los  conceptos que deseas que salgan en el repote";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 56);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 55);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
             // 
             // SeleccionarConceptosReporteActualizarInventario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(229, 313);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.CLBConceptosExistentes);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -47,5 +113,11 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckedListBox CLBConceptosExistentes;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
--- PuntoDeVentaV2/SeleccionarConceptosReporteActualizarInventario.cs
+++ PuntoDeVentaV2/SeleccionarConceptosReporteActualizarInventario.cs
@@ -12,9 +12,58 @@ namespace PuntoDeVentaV2
 {
     public partial class SeleccionarConceptosReporteActualizarInventario : Form
     {
+        int index = 0;
+        string[] itemsCLBConceptosExistentes;
+        List<string> listaConceptos;
+
+        private void unCheckAllListCLBExistentes()
+        {
+            for (int i = 0; i < CLBConceptosExistentes.Items.Count; i++)
+            {
+                CLBConceptosExistentes.SetItemChecked(i, false);
+            }
+        }
+
+        private void CheckAllListCLBExistentes()
+        {
+            for (int i = 0; i < CLBConceptosExistentes.Items.Count; i++)
+            {
+                CLBConceptosExistentes.SetItemChecked(i, true);
+            }
+        }
+
         public SeleccionarConceptosReporteActualizarInventario()
         {
             InitializeComponent();
+
+            itemsCLBConceptosExistentes = new string[] { "Producto", "Proveedor", "Unidades Compradas/Disminuidas", "Precio Compra", "Precio Venta", "Stock Anterior", "Stock Actual", "Fecha de Compra", "Fecha de Operacion", "Comentarios" };
+
+            CLBConceptosExistentes.Items.AddRange(itemsCLBConceptosExistentes);
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            listaConceptos = new List<string>();
+
+            if (!CLBConceptosExistentes.CheckedItems.Count.Equals(0))
+            {
+                for (int x = 0; x <= CLBConceptosExistentes.CheckedItems.Count - 1; x++)
+                {
+                    listaConceptos.Add(CLBConceptosExistentes.CheckedItems[x].ToString());
+                }
+            }
+            else if (CLBConceptosExistentes.CheckedItems.Count.Equals(0))
+            {
+                var cant = CLBConceptosExistentes.CheckedItems.Count;
+
+                MessageBox.Show("Cantidad de conceptos seleccionados debe de ser mayor a: " + cant + "\nFavor de seleccionar al menos uno de los conceptos", "Mensaje del Sistema", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            CLBConceptosExistentes.Items.Clear();
+            CLBConceptosExistentes.Items.AddRange(itemsCLBConceptosExistentes);
         }
     }
 }
