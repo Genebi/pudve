9be7fafb05693ca335801688132948e35aaf7940
Emmanuel <emmanuel.ruiz.garcia@outlook.com>
2021-03-04 09:22:27 -0600

    guardado temporal

--- PuntoDeVentaV2/AgregarEditarBascula.Designer.cs
+++ PuntoDeVentaV2/AgregarEditarBascula.Designer.cs
@@ -31,25 +31,29 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cbPuerto = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.cbBaudRate = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.cbDatos = new System.Windows.Forms.ComboBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.cbHandshake = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.cbParidad = new System.Windows.Forms.ComboBox();
             this.label7 = new System.Windows.Forms.Label();
-            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.cbStopBits = new System.Windows.Forms.ComboBox();
             this.label8 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtSendData = new System.Windows.Forms.TextBox();
             this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
+            this.lblPeso = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -71,150 +75,150 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(129, 71);
+            this.label2.Location = new System.Drawing.Point(129, 66);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(41, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Puerto:";
             // 
-            // comboBox1
+            // cbPuerto
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(197, 68);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(190, 21);
-            this.comboBox1.TabIndex = 3;
+            this.cbPuerto.FormattingEnabled = true;
+            this.cbPuerto.Location = new System.Drawing.Point(197, 63);
+            this.cbPuerto.Name = "cbPuerto";
+            this.cbPuerto.Size = new System.Drawing.Size(190, 21);
+            this.cbPuerto.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(434, 71);
+            this.label3.Location = new System.Drawing.Point(434, 66);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(58, 13);
             this.label3.TabIndex = 4;
             this.label3.Text = "BaudRate:";
             // 
-            // comboBox2
+            // cbBaudRate
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(519, 68);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(217, 21);
-            this.comboBox2.TabIndex = 5;
+            this.cbBaudRate.FormattingEnabled = true;
+            this.cbBaudRate.Location = new System.Drawing.Point(519, 63);
+            this.cbBaudRate.Name = "cbBaudRate";
+            this.cbBaudRate.Size = new System.Drawing.Size(217, 21);
+            this.cbBaudRate.TabIndex = 5;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(120, 117);
+            this.label4.Location = new System.Drawing.Point(120, 105);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(50, 13);
             this.label4.TabIndex = 6;
             this.label4.Text = "DataBits:";
             // 
-            // comboBox3
+            // cbDatos
             // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(197, 114);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(190, 21);
-            this.comboBox3.TabIndex = 7;
+            this.cbDatos.FormattingEnabled = true;
+            this.cbDatos.Location = new System.Drawing.Point(197, 102);
+            this.cbDatos.Name = "cbDatos";
+            this.cbDatos.Size = new System.Drawing.Size(190, 21);
+            this.cbDatos.TabIndex = 7;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(427, 117);
+            this.label5.Location = new System.Drawing.Point(427, 105);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(65, 13);
             this.label5.TabIndex = 8;
             this.label5.Text = "Handshake:";
             // 
-            // comboBox4
+            // cbHandshake
             // 
-            this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(519, 114);
-            this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(217, 21);
-            this.comboBox4.TabIndex = 9;
+            this.cbHandshake.FormattingEnabled = true;
+            this.cbHandshake.Location = new System.Drawing.Point(519, 102);
+            this.cbHandshake.Name = "cbHandshake";
+            this.cbHandshake.Size = new System.Drawing.Size(217, 21);
+            this.cbHandshake.TabIndex = 9;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(134, 165);
+            this.label6.Location = new System.Drawing.Point(134, 143);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(36, 13);
             this.label6.TabIndex = 10;
             this.label6.Text = "Parity:";
             // 
-            // comboBox5
+            // cbParidad
             // 
-            this.comboBox5.FormattingEnabled = true;
-            this.comboBox5.Location = new System.Drawing.Point(197, 162);
-            this.comboBox5.Name = "comboBox5";
-            this.comboBox5.Size = new System.Drawing.Size(190, 21);
-            this.comboBox5.TabIndex = 11;
+            this.cbParidad.FormattingEnabled = true;
+            this.cbParidad.Location = new System.Drawing.Point(197, 140);
+            this.cbParidad.Name = "cbParidad";
+            this.cbParidad.Size = new System.Drawing.Size(190, 21);
+            this.cbParidad.TabIndex = 11;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(443, 165);
+            this.label7.Location = new System.Drawing.Point(443, 143);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(49, 13);
             this.label7.TabIndex = 12;
             this.label7.Text = "StopBits:";
             // 
-            // comboBox6
+            // cbStopBits
             // 
-            this.comboBox6.FormattingEnabled = true;
-            this.comboBox6.Location = new System.Drawing.Point(519, 162);
-            this.comboBox6.Name = "comboBox6";
-            this.comboBox6.Size = new System.Drawing.Size(217, 21);
-            this.comboBox6.TabIndex = 13;
+            this.cbStopBits.FormattingEnabled = true;
+            this.cbStopBits.Location = new System.Drawing.Point(519, 140);
+            this.cbStopBits.Name = "cbStopBits";
+            this.cbStopBits.Size = new System.Drawing.Size(217, 21);
+            this.cbStopBits.TabIndex = 13;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(104, 215);
+            this.label8.Location = new System.Drawing.Point(104, 182);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(66, 13);
             this.label8.TabIndex = 14;
             this.label8.Text = "Enviar Dato:";
             // 
-            // textBox2
+            // txtSendData
             // 
-            this.textBox2.Location = new System.Drawing.Point(197, 212);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(190, 20);
-            this.textBox2.TabIndex = 15;
+            this.txtSendData.Location = new System.Drawing.Point(197, 179);
+            this.txtSendData.Name = "txtSendData";
+            this.txtSendData.Size = new System.Drawing.Size(190, 20);
+            this.txtSendData.TabIndex = 15;
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(458, 215);
+            this.label9.Location = new System.Drawing.Point(458, 182);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(34, 13);
             this.label9.TabIndex = 16;
             this.label9.Text = "Peso:";
             // 
-            // label10
+            // lblPeso
             // 
-            this.label10.BackColor = System.Drawing.Color.White;
-            this.label10.Font = new System.Drawing.Font("Century", 12F);
-            this.label10.Location = new System.Drawing.Point(519, 209);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(217, 23);
-            this.label10.TabIndex = 17;
-            this.label10.Text = "0";
-            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblPeso.BackColor = System.Drawing.Color.White;
+            this.lblPeso.Font = new System.Drawing.Font("Century", 12F);
+            this.lblPeso.Location = new System.Drawing.Point(519, 176);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(217, 23);
+            this.lblPeso.TabIndex = 17;
+            this.lblPeso.Text = "0";
+            this.lblPeso.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Image = global::PuntoDeVentaV2.Properties.Resources.plus_square1;
-            this.button1.Location = new System.Drawing.Point(26, 268);
+            this.button1.Image = global::PuntoDeVentaV2.Properties.Resources.plus_square_o1;
+            this.button1.Location = new System.Drawing.Point(26, 228);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(162, 69);
             this.button1.TabIndex = 18;
-            this.button1.Text = "Agregar / Atualizar";
+            this.button1.Text = "Agregar";
             this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.button1.UseVisualStyleBackColor = true;
             // 
@@ -222,7 +226,7 @@
             // 
             this.button2.Font = new System.Drawing.Font("Century", 12F);
             this.button2.Image = global::PuntoDeVentaV2.Properties.Resources.save1;
-            this.button2.Location = new System.Drawing.Point(211, 269);
+            this.button2.Location = new System.Drawing.Point(211, 229);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(162, 69);
             this.button2.TabIndex = 19;
@@ -233,7 +237,8 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Century", 12F);
-            this.button3.Location = new System.Drawing.Point(394, 268);
+            this.button3.Image = global::PuntoDeVentaV2.Properties.Resources.scale1;
+            this.button3.Location = new System.Drawing.Point(394, 228);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(162, 69);
             this.button3.TabIndex = 20;
@@ -244,7 +249,8 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Century", 12F);
-            this.button4.Location = new System.Drawing.Point(574, 268);
+            this.button4.Image = global::PuntoDeVentaV2.Properties.Resources.cogs1;
+            this.button4.Location = new System.Drawing.Point(574, 228);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(162, 69);
             this.button4.TabIndex = 21;
@@ -252,30 +258,49 @@
             this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.button4.UseVisualStyleBackColor = true;
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(761, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(301, 269);
+            this.groupBox1.TabIndex = 22;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = " Basculas Registradas ";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(19, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(268, 231);
+            this.dataGridView1.TabIndex = 0;
+            // 
             // AgregarEditarBascula
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(775, 349);
+            this.ClientSize = new System.Drawing.Size(1082, 318);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lblPeso);
             this.Controls.Add(this.label9);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtSendData);
             this.Controls.Add(this.label8);
-            this.Controls.Add(this.comboBox6);
+            this.Controls.Add(this.cbStopBits);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.cbParidad);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.cbHandshake);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.cbDatos);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.cbBaudRate);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cbPuerto);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
@@ -286,6 +311,9 @@
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Agregar o editar basculas en el sistema";
+            this.Load += new System.EventHandler(this.AgregarEditarBascula_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -296,24 +324,26 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cbPuerto;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox cbBaudRate;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox cbDatos;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox cbHandshake;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox cbParidad;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.ComboBox cbStopBits;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtSendData;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lblPeso;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- PuntoDeVentaV2/Properties/Resources.Designer.cs
+++ PuntoDeVentaV2/Properties/Resources.Designer.cs
@@ -12276,6 +12276,16 @@ namespace PuntoDeVentaV2.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap scale1 {
+            get {
+                object obj = ResourceManager.GetObject("scale1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap scissors {
             get {
                 object obj = ResourceManager.GetObject("scissors", resourceCulture);
