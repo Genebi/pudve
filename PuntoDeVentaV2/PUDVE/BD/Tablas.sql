cc20ec1dcae6af1a649cce0e82b80809c6aec3ca
Emmanuel <emmanuel.ruiz.garcia@outlook.com>
2021-03-12 12:18:26 -0600

    se agrego el cdigo MySql al archivo de Tablas para que haga la tabla en la base de datos

--- PuntoDeVentaV2/PUDVE/BD/Tablas.sql
+++ PuntoDeVentaV2/PUDVE/BD/Tablas.sql
@@ -949,7 +949,7 @@ IF
 CREATE TABLE
 IF
 	NOT EXISTS basculas (
-		idBascula INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+		idBascula INT ( 11 ) NOT NULL AUTO_INCREMENT,
 		nombreBascula VARCHAR ( 255 ) NOT NULL,
 		puerto VARCHAR ( 100 ) NOT NULL,
 		baudRate VARCHAR ( 100 ) NOT NULL,
@@ -958,9 +958,10 @@ IF
 		parity VARCHAR ( 100 ) NOT NULL,
 		stopBits VARCHAR ( 100 ) NOT NULL,
 		sendData VARCHAR ( 100 ) NOT NULL,
-		idUsuario INTEGER NOT NULL,
-		predeterminada INTEGER NOT NULL DEFAULT (0),
-		UNIQUE INDEX unique_NombreBascula ( nombreBascula ),
+		idUsuario INT ( 11 ) NOT NULL,
+		predeterminada INT ( 11 ) NOT NULL DEFAULT 0,
+		PRIMARY KEY ( idBascula ),
+		UNIQUE ( nombreBascula, idUsuario, predeterminada ),
 	CONSTRAINT Basculas_IdUsuario FOREIGN KEY ( idUsuario ) REFERENCES usuarios ( ID ) ON DELETE CASCADE ON UPDATE CASCADE 
 	);
 
