9312acfe832f9ce2109c12d22c3310aab28d8395
Emmanuel <emmanuel.ruiz.garcia@outlook.com>
2021-04-22 15:44:45 -0500

    correción de botón primera pagina del listado desde seleccion de producto, Combo, Servicio desde la sección de productos

--- PuntoDeVentaV2/ListaProductos.cs
+++ PuntoDeVentaV2/ListaProductos.cs
@@ -353,6 +353,7 @@ namespace PuntoDeVentaV2
         private void btnPrimeraPagina_Click(object sender, EventArgs e)
         {
             p.primerPagina();
+            clickBoton = 1;
             CargarDatos();
             actualizar();
         }
