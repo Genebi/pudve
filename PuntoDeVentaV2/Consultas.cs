0ef541319d220c72b44597bea13ecef1f985a169
Emmanuel <emmanuel.ruiz.garcia@outlook.com>
2021-05-21 14:02:34 -0500

    reacomodo del archivo clase Consultas

--- PuntoDeVentaV2/Consultas.cs
+++ PuntoDeVentaV2/Consultas.cs
@@ -1732,8 +1732,7 @@ namespace PuntoDeVentaV2
 
         public string ObtenerProdDeLaVenta(int idVenta)
         {
-            var consulta = $@"SELECT DISTINCT Prod.ID, Prod.Nombre, Prod.Stock
-FROM detallesventa AS SaleDetail INNER JOIN ventas AS Sale ON Sale.ID = SaleDetail.IDVenta INNER JOIN usuarios AS Usr ON Usr.ID = Sale.IDUsuario INNER JOIN productosventa AS SaleProd ON SaleProd.IDVenta = Sale.ID  INNER JOIN productos AS Prod ON Prod.ID = SaleProd.IDProducto WHERE SaleDetail.IDVenta = '{idVenta}' GROUP BY Prod.ID";
+            var consulta = $@"SELECT DISTINCT Prod.ID, Prod.Nombre, Prod.Stock FROM detallesventa AS SaleDetail INNER JOIN ventas AS Sale ON Sale.ID = SaleDetail.IDVenta INNER JOIN usuarios AS Usr ON Usr.ID = Sale.IDUsuario INNER JOIN productosventa AS SaleProd ON SaleProd.IDVenta = Sale.ID  INNER JOIN productos AS Prod ON Prod.ID = SaleProd.IDProducto WHERE SaleDetail.IDVenta = '{idVenta}' GROUP BY Prod.ID";
 
             return consulta;
         }
