5230ef3bb6cc5aa4e17e15f2fac845c9ebee0bbf
Emmanuel Ruiz García <emmanuel.ruiz.garcia@outlook.com>
2021-07-30 13:12:48 -0500

    se verifico  ver la lista de relaciones de proudctos primera etapa

--- PuntoDeVentaV2/AgregarEditarProducto.Designer.cs
+++ PuntoDeVentaV2/AgregarEditarProducto.Designer.cs
@@ -932,7 +932,7 @@
             this.fLPType.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
             this.fLPType.Location = new System.Drawing.Point(0, 0);
             this.fLPType.Name = "fLPType";
-            this.fLPType.Size = new System.Drawing.Size(921, 29);
+            this.fLPType.Size = new System.Drawing.Size(938, 29);
             this.fLPType.TabIndex = 35;
             // 
             // fLPDetalleProducto
@@ -942,7 +942,7 @@
             this.fLPDetalleProducto.Dock = System.Windows.Forms.DockStyle.Top;
             this.fLPDetalleProducto.Location = new System.Drawing.Point(0, 29);
             this.fLPDetalleProducto.Name = "fLPDetalleProducto";
-            this.fLPDetalleProducto.Size = new System.Drawing.Size(921, 66);
+            this.fLPDetalleProducto.Size = new System.Drawing.Size(938, 66);
             this.fLPDetalleProducto.TabIndex = 36;
             this.fLPDetalleProducto.Paint += new System.Windows.Forms.PaintEventHandler(this.fLPDetalleProducto_Paint);
             // 
@@ -964,7 +964,7 @@
             this.fLPDetalle.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
             this.fLPDetalle.Location = new System.Drawing.Point(0, 95);
             this.fLPDetalle.Name = "fLPDetalle";
-            this.fLPDetalle.Size = new System.Drawing.Size(921, 210);
+            this.fLPDetalle.Size = new System.Drawing.Size(938, 210);
             this.fLPDetalle.TabIndex = 38;
             // 
             // fLPAccion
@@ -984,7 +984,7 @@
             this.fLPDetallesProducto.Dock = System.Windows.Forms.DockStyle.Top;
             this.fLPDetallesProducto.Location = new System.Drawing.Point(0, 305);
             this.fLPDetallesProducto.Name = "fLPDetallesProducto";
-            this.fLPDetallesProducto.Size = new System.Drawing.Size(921, 270);
+            this.fLPDetallesProducto.Size = new System.Drawing.Size(938, 270);
             this.fLPDetallesProducto.TabIndex = 39;
             // 
             // groupBox2
@@ -1033,7 +1033,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(938, 680);
+            this.ClientSize = new System.Drawing.Size(955, 680);
             this.Controls.Add(this.fLPContenidoProducto);
             this.Controls.Add(this.fLPDetallesProducto);
             this.Controls.Add(this.fLPAccion);
