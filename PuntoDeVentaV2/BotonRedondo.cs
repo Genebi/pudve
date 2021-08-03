fd1a9a449c5c6a5e82ba7a956073bac55ec0d666
Emmanuel Ruiz García <emmanuel.ruiz.garcia@outlook.com>
2021-08-03 12:26:56 -0500

    se agrego la clase que se hara el control personalizado para los botones redondos

---
+++ PuntoDeVentaV2/BotonRedondo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.ComponentModel;
+
+namespace PuntoDeVentaV2
+{
+    public class BotonRedondo : Button
+    {
+
+    }
+}
