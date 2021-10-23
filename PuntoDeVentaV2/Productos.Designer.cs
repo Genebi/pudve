f048793b11fd861326bf2bd5eabf4eac2638efd1
Muelas0 <alonso_2nov@hotmail.com>
2021-10-23 11:39:35 -0500

    Se me olvido agregar el audio

--- PuntoDeVentaV2/Productos.Designer.cs
+++ PuntoDeVentaV2/Productos.Designer.cs
@@ -111,6 +111,7 @@
             this.lblCantidadRegistros = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
             this.panelShowDGVProductosView.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DGVProductos)).BeginInit();
             this.panelShowPhotoView.SuspendLayout();
@@ -150,7 +151,7 @@
             this.tituloSeccion.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.tituloSeccion.AutoSize = true;
             this.tituloSeccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tituloSeccion.Location = new System.Drawing.Point(296, 1);
+            this.tituloSeccion.Location = new System.Drawing.Point(338, 1);
             this.tituloSeccion.Name = "tituloSeccion";
             this.tituloSeccion.Size = new System.Drawing.Size(418, 25);
             this.tituloSeccion.TabIndex = 3;
@@ -176,7 +177,7 @@
             this.txtBusqueda.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtBusqueda.Location = new System.Drawing.Point(173, 133);
             this.txtBusqueda.Name = "txtBusqueda";
-            this.txtBusqueda.Size = new System.Drawing.Size(679, 22);
+            this.txtBusqueda.Size = new System.Drawing.Size(763, 22);
             this.txtBusqueda.TabIndex = 5;
             this.txtBusqueda.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.txtBusqueda.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtBusqueda_KeyDown);
@@ -231,7 +232,7 @@
             this.panelShowDGVProductosView.Location = new System.Drawing.Point(13, 225);
             this.panelShowDGVProductosView.Margin = new System.Windows.Forms.Padding(2);
             this.panelShowDGVProductosView.Name = "panelShowDGVProductosView";
-            this.panelShowDGVProductosView.Size = new System.Drawing.Size(940, 317);
+            this.panelShowDGVProductosView.Size = new System.Drawing.Size(1024, 317);
             this.panelShowDGVProductosView.TabIndex = 13;
             // 
             // DGVProductos
@@ -274,7 +275,7 @@
             this.DGVProductos.ReadOnly = true;
             this.DGVProductos.RowHeadersVisible = false;
             this.DGVProductos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.DGVProductos.Size = new System.Drawing.Size(918, 308);
+            this.DGVProductos.Size = new System.Drawing.Size(1002, 308);
             this.DGVProductos.TabIndex = 2;
             this.DGVProductos.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGVProductos_CellClick);
             this.DGVProductos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGVProductos_CellContentClick);
@@ -479,7 +480,7 @@
             this.panelShowPhotoView.Location = new System.Drawing.Point(12, 225);
             this.panelShowPhotoView.Margin = new System.Windows.Forms.Padding(2);
             this.panelShowPhotoView.Name = "panelShowPhotoView";
-            this.panelShowPhotoView.Size = new System.Drawing.Size(939, 376);
+            this.panelShowPhotoView.Size = new System.Drawing.Size(1023, 376);
             this.panelShowPhotoView.TabIndex = 14;
             // 
             // fLPShowPhoto
@@ -491,7 +492,7 @@
             this.fLPShowPhoto.Location = new System.Drawing.Point(10, 5);
             this.fLPShowPhoto.Margin = new System.Windows.Forms.Padding(2);
             this.fLPShowPhoto.Name = "fLPShowPhoto";
-            this.fLPShowPhoto.Size = new System.Drawing.Size(917, 310);
+            this.fLPShowPhoto.Size = new System.Drawing.Size(1001, 310);
             this.fLPShowPhoto.TabIndex = 0;
             // 
             // panel1
@@ -520,7 +521,7 @@
             this.panel1.Location = new System.Drawing.Point(12, 6);
             this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(942, 215);
+            this.panel1.Size = new System.Drawing.Size(1026, 215);
             this.panel1.TabIndex = 15;
             // 
             // groupBox3
@@ -603,7 +604,7 @@
             this.lAtajo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.lAtajo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lAtajo.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.lAtajo.Location = new System.Drawing.Point(841, 5);
+            this.lAtajo.Location = new System.Drawing.Point(925, 5);
             this.lAtajo.Name = "lAtajo";
             this.lAtajo.Size = new System.Drawing.Size(90, 23);
             this.lAtajo.TabIndex = 24;
@@ -633,7 +634,7 @@
             this.btnCleanFilter.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.btnCleanFilter.ForeColor = System.Drawing.Color.White;
             this.btnCleanFilter.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnCleanFilter.Location = new System.Drawing.Point(861, 158);
+            this.btnCleanFilter.Location = new System.Drawing.Point(945, 158);
             this.btnCleanFilter.Name = "btnCleanFilter";
             this.btnCleanFilter.Size = new System.Drawing.Size(68, 23);
             this.btnCleanFilter.TabIndex = 21;
@@ -679,7 +680,7 @@
             this.fLPDynamicTags.BackColor = System.Drawing.SystemColors.Control;
             this.fLPDynamicTags.Location = new System.Drawing.Point(173, 156);
             this.fLPDynamicTags.Name = "fLPDynamicTags";
-            this.fLPDynamicTags.Size = new System.Drawing.Size(679, 30);
+            this.fLPDynamicTags.Size = new System.Drawing.Size(763, 30);
             this.fLPDynamicTags.TabIndex = 18;
             // 
             // btnEtiqueta
@@ -727,7 +728,7 @@
             this.btnFilterSearch.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.btnFilterSearch.ForeColor = System.Drawing.Color.White;
             this.btnFilterSearch.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnFilterSearch.Location = new System.Drawing.Point(861, 133);
+            this.btnFilterSearch.Location = new System.Drawing.Point(945, 133);
             this.btnFilterSearch.Name = "btnFilterSearch";
             this.btnFilterSearch.Size = new System.Drawing.Size(68, 23);
             this.btnFilterSearch.TabIndex = 15;
@@ -754,7 +755,7 @@
             this.groupBox1.Controls.Add(this.btnAgregarPaquete);
             this.groupBox1.Controls.Add(this.btnAgregarProducto);
             this.groupBox1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(590, 47);
+            this.groupBox1.Location = new System.Drawing.Point(674, 47);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(339, 54);
             this.groupBox1.TabIndex = 13;
@@ -851,6 +852,7 @@
             // 
             this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.button1);
             this.panel2.Controls.Add(this.lbCantidadSeleccionada);
             this.panel2.Controls.Add(this.lbCapital);
             this.panel2.Controls.Add(this.lbPaginasSeleccionadas);
@@ -868,7 +870,7 @@
             this.panel2.Controls.Add(this.label1);
             this.panel2.Location = new System.Drawing.Point(12, 542);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(941, 65);
+            this.panel2.Size = new System.Drawing.Size(1025, 65);
             this.panel2.TabIndex = 17;
             // 
             // lbCantidadSeleccionada
@@ -904,7 +906,7 @@
             // 
             this.txtIrPagina.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.txtIrPagina.ForeColor = System.Drawing.Color.Blue;
-            this.txtIrPagina.Location = new System.Drawing.Point(426, 40);
+            this.txtIrPagina.Location = new System.Drawing.Point(468, 40);
             this.txtIrPagina.Name = "txtIrPagina";
             this.txtIrPagina.Size = new System.Drawing.Size(66, 20);
             this.txtIrPagina.TabIndex = 24;
@@ -917,7 +919,7 @@
             // 
             this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(361, 44);
+            this.label3.Location = new System.Drawing.Point(403, 44);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(61, 13);
             this.label3.TabIndex = 23;
@@ -929,7 +931,7 @@
             this.panel4.Controls.Add(this.linkLblUltimaPagina);
             this.panel4.Controls.Add(this.btnSiguiente);
             this.panel4.Controls.Add(this.btnUltimaPagina);
-            this.panel4.Location = new System.Drawing.Point(498, 8);
+            this.panel4.Location = new System.Drawing.Point(540, 8);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(59, 32);
             this.panel4.TabIndex = 21;
@@ -983,7 +985,7 @@
             this.panel3.Controls.Add(this.btnPrimeraPagina);
             this.panel3.Controls.Add(this.btnAnterior);
             this.panel3.Controls.Add(this.linkLblPrimeraPagina);
-            this.panel3.Location = new System.Drawing.Point(364, 8);
+            this.panel3.Location = new System.Drawing.Point(406, 8);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(56, 32);
             this.panel3.TabIndex = 20;
@@ -1037,7 +1039,7 @@
             this.linkLblPaginaSiguiente.AutoSize = true;
             this.linkLblPaginaSiguiente.Cursor = System.Windows.Forms.Cursors.Hand;
             this.linkLblPaginaSiguiente.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkLblPaginaSiguiente.Location = new System.Drawing.Point(475, 18);
+            this.linkLblPaginaSiguiente.Location = new System.Drawing.Point(517, 18);
             this.linkLblPaginaSiguiente.Name = "linkLblPaginaSiguiente";
             this.linkLblPaginaSiguiente.Size = new System.Drawing.Size(15, 16);
             this.linkLblPaginaSiguiente.TabIndex = 16;
@@ -1051,7 +1053,7 @@
             this.linkLblPaginaActual.AutoSize = true;
             this.linkLblPaginaActual.Cursor = System.Windows.Forms.Cursors.Hand;
             this.linkLblPaginaActual.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkLblPaginaActual.Location = new System.Drawing.Point(452, 18);
+            this.linkLblPaginaActual.Location = new System.Drawing.Point(494, 18);
             this.linkLblPaginaActual.Name = "linkLblPaginaActual";
             this.linkLblPaginaActual.Size = new System.Drawing.Size(15, 16);
             this.linkLblPaginaActual.TabIndex = 15;
@@ -1065,7 +1067,7 @@
             this.linkLblPaginaAnterior.AutoSize = true;
             this.linkLblPaginaAnterior.Cursor = System.Windows.Forms.Cursors.Hand;
             this.linkLblPaginaAnterior.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkLblPaginaAnterior.Location = new System.Drawing.Point(428, 18);
+            this.linkLblPaginaAnterior.Location = new System.Drawing.Point(470, 18);
             this.linkLblPaginaAnterior.Name = "linkLblPaginaAnterior";
             this.linkLblPaginaAnterior.Size = new System.Drawing.Size(15, 16);
             this.linkLblPaginaAnterior.TabIndex = 14;
@@ -1082,7 +1084,7 @@
             this.btnActualizarMaximoProductos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnActualizarMaximoProductos.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.btnActualizarMaximoProductos.ForeColor = System.Drawing.Color.Black;
-            this.btnActualizarMaximoProductos.Location = new System.Drawing.Point(764, 13);
+            this.btnActualizarMaximoProductos.Location = new System.Drawing.Point(806, 13);
             this.btnActualizarMaximoProductos.Name = "btnActualizarMaximoProductos";
             this.btnActualizarMaximoProductos.Size = new System.Drawing.Size(80, 23);
             this.btnActualizarMaximoProductos.TabIndex = 8;
@@ -1093,7 +1095,7 @@
             // txtMaximoPorPagina
             // 
             this.txtMaximoPorPagina.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.txtMaximoPorPagina.Location = new System.Drawing.Point(698, 14);
+            this.txtMaximoPorPagina.Location = new System.Drawing.Point(740, 14);
             this.txtMaximoPorPagina.Name = "txtMaximoPorPagina";
             this.txtMaximoPorPagina.Size = new System.Drawing.Size(56, 20);
             this.txtMaximoPorPagina.TabIndex = 7;
@@ -1104,7 +1106,7 @@
             // label7
             // 
             this.label7.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.label7.Location = new System.Drawing.Point(571, 8);
+            this.label7.Location = new System.Drawing.Point(613, 8);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(116, 32);
             this.label7.TabIndex = 6;
@@ -1116,7 +1118,7 @@
             this.lblCantidadRegistros.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.lblCantidadRegistros.BackColor = System.Drawing.SystemColors.Control;
             this.lblCantidadRegistros.ForeColor = System.Drawing.Color.Blue;
-            this.lblCantidadRegistros.Location = new System.Drawing.Point(295, 13);
+            this.lblCantidadRegistros.Location = new System.Drawing.Point(337, 13);
             this.lblCantidadRegistros.Name = "lblCantidadRegistros";
             this.lblCantidadRegistros.Size = new System.Drawing.Size(62, 23);
             this.lblCantidadRegistros.TabIndex = 1;
@@ -1127,7 +1129,7 @@
             // 
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(191, 11);
+            this.label1.Location = new System.Drawing.Point(233, 11);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(99, 26);
             this.label1.TabIndex = 0;
@@ -1139,11 +1141,20 @@
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(945, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 28;
+            this.button1.Text = "Numero de Orden";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Productos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(965, 611);
+            this.ClientSize = new System.Drawing.Size(1049, 611);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panelShowDGVProductosView);
@@ -1256,5 +1267,6 @@
         private System.Windows.Forms.Label lbCapital;
         private System.Windows.Forms.Label lbCantidadSeleccionada;
         private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button1;
     }
 }
