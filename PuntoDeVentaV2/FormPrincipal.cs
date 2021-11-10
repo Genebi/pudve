7c32a2cbfd273ddcd8701e1286c03eb82b0b1579
Emmanuel Ruiz García <emmanuel.ruiz.garcia@outlook.com>
2021-11-10 15:57:04 -0600

    solución reinicio de sistema

--- PuntoDeVentaV2/FormPrincipal.cs
+++ PuntoDeVentaV2/FormPrincipal.cs
@@ -1242,8 +1242,8 @@ namespace PuntoDeVentaV2
             validarRespaldoBaseDeDatos();
             cerrarAplicacion = false;
 
-            //Application.Restart();
-            //Environment.Exit(0);
+            Application.Restart();
+            Environment.Exit(0);
         }
 
         private void validarRespaldoBaseDeDatos()
