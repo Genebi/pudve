cb926f292b0338fa8b5eabb5a512af08ac0bc41e
Emmanuel Ruiz García <emmanuel.ruiz.garcia@outlook.com>
2021-11-30 10:12:56 -0600

    verificación de estructura del reporte de productos menos vendidos

--- PuntoDeVentaV2/Consultas.cs
+++ PuntoDeVentaV2/Consultas.cs
@@ -2971,7 +2971,7 @@ namespace PuntoDeVentaV2
 
         public string productosMenosVendidos(string fechaInicio, string fechaFinal)
         {
-            var consulta = $"SELECT Prod.ID, Prod.Nombre AS 'ARTICULO', SUM( DISTINCT IF ( ProdVent.Cantidad IS NULL, 0, ProdVent.Cantidad ) ) AS 'VENDIDOS', IF ( Prod.Tipo = 'P', 'PRODUCTO', 'SERVICIO' ) AS 'CATEGORIA', IF ( Vent.FechaOperacion IS NULL, 'N/A sin venta registrada', Vent.FechaOperacion ) AS 'ULTIMA VENTA', Prod.Stock AS 'STOCK', Prod.Precio AS 'PRECIO' FROM Productos AS Prod LEFT JOIN ProductosVenta AS ProdVent ON ( ProdVent.IDProducto = Prod.ID ) LEFT JOIN Ventas AS Vent ON ( Vent.ID = ProdVent.IDVenta ) WHERE Vent.IDUsuario = '{FormPrincipal.userID}' AND Vent.FechaOperacion BETWEEN '{fechaInicio}.999999' AND '{fechaFinal}.999999' AND ( Prod.Tipo = 'P' || Prod.Tipo = 'S' ) OR ProdVent.Cantidad IS NULL GROUP BY Prod.ID ORDER BY VENDIDOS ASC";
+            var consulta = $"SELECT Prod.ID, Prod.Nombre AS 'ARTICULO', SUM( DISTINCT IF ( ProdVent.Cantidad IS NULL, 0, ProdVent.Cantidad ) ) AS 'VENDIDOS',Prod.CodigoBarras AS 'CODIGO DE BARRAS', IF( Prod.Tipo = 'P', 'PRODUCTO', 'SERVICIO' ) AS 'CATEGORIA', IF ( Vent.FechaOperacion IS NULL, 'N/A sin venta registrada', Vent.FechaOperacion ) AS 'ULTIMA VENTA', Prod.Stock AS 'STOCK', Prod.Precio AS 'PRECIO' FROM Productos AS Prod LEFT JOIN ProductosVenta AS ProdVent ON ( ProdVent.IDProducto = Prod.ID ) LEFT JOIN Ventas AS Vent ON ( Vent.ID = ProdVent.IDVenta ) WHERE Vent.IDUsuario = '{FormPrincipal.userID}' AND Vent.FechaOperacion BETWEEN '{fechaInicio}.999999' AND '{fechaFinal}.999999' AND ( Prod.Tipo = 'P' || Prod.Tipo = 'S' ) OR ProdVent.Cantidad IS NULL GROUP BY Prod.ID ORDER BY VENDIDOS ASC";
 
             return consulta;
         }
--- PuntoDeVentaV2/RangosReporteProductosMenosVendidos.cs
+++ PuntoDeVentaV2/RangosReporteProductosMenosVendidos.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -82,7 +85,220 @@ namespace PuntoDeVentaV2
 
         private void generarReporteMenosVendidos(DataTable dtProductosMenosVendidos)
         {
-            
+            var servidor = Properties.Settings.Default.Hosting;
+
+            var rutaArchivo = string.Empty;
+            var usuario = string.Empty;
+
+            if (FormPrincipal.userNickName.Contains("@"))
+            {
+                var palabras = FormPrincipal.userNickName.Split('@');
+                usuario = palabras[0].ToString();
+            }
+            else
+            {
+                usuario = FormPrincipal.userNickName;
+            }
+
+            if (!string.IsNullOrWhiteSpace(servidor))
+            {
+                rutaArchivo = $@"\\{servidor}\Archivos PUDVE\Reportes\Historial\{usuario}\MenosVendidos\";
+            }
+            else
+            {
+                rutaArchivo = $@"C:\Archivos PUDVE\Reportes\Historial\{usuario}\MenosVendidos\";
+            }
+
+            if (!Directory.Exists(rutaArchivo))
+            {
+                Directory.CreateDirectory(rutaArchivo);
+            }
+
+            if (!string.IsNullOrWhiteSpace(servidor))
+            {
+                rutaArchivo += $"reporte_Menos_Vendidos.pdf";
+            }
+            else
+            {
+                rutaArchivo += $"reporte_Menos_Vendidos.pdf";
+            }
+
+            // Datos del usuario
+            var datos = FormPrincipal.datosUsuario;
+
+            // Fuentes y Colores
+            var colorFuenteNegrita = new BaseColor(Color.Black);
+            var colorFuenteBlanca = new BaseColor(Color.White);
+
+            var fuenteNormal = FontFactory.GetFont(FontFactory.HELVETICA, 8);
+            var fuenteNegrita = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 8, 1, colorFuenteNegrita);
+            var fuenteGrande = FontFactory.GetFont(FontFactory.HELVETICA, 10);
+            var fuenteMensaje = FontFactory.GetFont(FontFactory.HELVETICA, 10);
+            var fuenteTotales = FontFactory.GetFont(FontFactory.HELVETICA, 8, 1, colorFuenteNegrita);
+
+            var numRow = 0;
+
+            Document reporte = new Document(PageSize.A3.Rotate());
+            PdfWriter writer = PdfWriter.GetInstance(reporte, new FileStream(rutaArchivo, FileMode.Create));
+
+            reporte.Open();
+
+            Paragraph titulo = new Paragraph(datos[0], fuenteGrande);
+
+            Paragraph Usuario = new Paragraph("");
+
+            string UsuarioActivo = string.Empty;
+
+            using (DataTable dtDataUsr = cn.CargarDatos(cs.UsuarioRazonSocialNombreCompleto(Convert.ToString(FormPrincipal.userID))))
+            {
+                if (!dtDataUsr.Rows.Count.Equals(0))
+                {
+                    foreach (DataRow drDataUsr in dtDataUsr.Rows)
+                    {
+                        UsuarioActivo = drDataUsr["Usuario"].ToString();
+                    }
+                }
+            }
+
+            Usuario = new Paragraph("USUARIO: " + UsuarioActivo, fuenteNegrita);
+
+            Paragraph subTitulo = new Paragraph("REPORTE PRODUCTO MENOS VENDIDO\n\nFechas consultadas\ndesde: " + fechaHoraInicio + "\nhasta: " + fechaHoraFinal + " \nCantidad de productos mostrados: " + cantidadLimite.ToString("N0") + "\n\n\n", fuenteNormal);
+
+            titulo.Alignment = Element.ALIGN_CENTER;
+            Usuario.Alignment = Element.ALIGN_CENTER;
+            subTitulo.Alignment = Element.ALIGN_CENTER;
+
+            float[] anchoColumnas = new float[] { 40f, 400f, 80f, 80f, 80f, 200f, 80f, 80f };
+
+            PdfPTable tablaInventario = new PdfPTable(8);
+            tablaInventario.WidthPercentage = 100;
+            tablaInventario.SetWidths(anchoColumnas);
+
+            PdfPCell colNoConcepto = new PdfPCell(new Phrase("No:", fuenteNegrita));
+            colNoConcepto.BorderWidth = 0;
+            colNoConcepto.BackgroundColor = new BaseColor(Color.SkyBlue);
+            colNoConcepto.HorizontalAlignment = Element.ALIGN_LEFT;
+
+            PdfPCell colNombre = new PdfPCell(new Phrase("ARTICULO", fuenteTotales));
+            colNombre.BorderWidth = 0;
+            colNombre.HorizontalAlignment = Element.ALIGN_LEFT;
+            colNombre.Padding = 3;
+            colNombre.BackgroundColor = new BaseColor(Color.SkyBlue);
+
+            PdfPCell colVendidos = new PdfPCell(new Phrase("VENDIDOS", fuenteTotales));
+            colVendidos.BorderWidth = 0;
+            colVendidos.HorizontalAlignment = Element.ALIGN_CENTER;
+            colVendidos.Padding = 3;
+            colVendidos.BackgroundColor = new BaseColor(Color.SkyBlue);
+
+            PdfPCell colCodigoBarra = new PdfPCell(new Phrase("CODIGO DE BARRAS", fuenteTotales));
+            colCodigoBarra.BorderWidth = 0;
+            colCodigoBarra.HorizontalAlignment = Element.ALIGN_CENTER;
+            colCodigoBarra.Padding = 3;
+            colCodigoBarra.BackgroundColor = new BaseColor(Color.SkyBlue);
+
+            PdfPCell colCategoria = new PdfPCell(new Phrase("CATEGORIA", fuenteTotales));
+            colCategoria.BorderWidth = 0;
+            colCategoria.HorizontalAlignment = Element.ALIGN_CENTER;
+            colCategoria.Padding = 3;
+            colCategoria.BackgroundColor = new BaseColor(Color.SkyBlue);
+
+            PdfPCell colUltimaVenta = new PdfPCell(new Phrase("ULTIMA VENTA", fuenteTotales));
+            colUltimaVenta.BorderWidth = 0;
+            colUltimaVenta.HorizontalAlignment = Element.ALIGN_CENTER;
+            colUltimaVenta.Padding = 3;
+            colUltimaVenta.BackgroundColor = new BaseColor(Color.SkyBlue);
+
+            PdfPCell colStock = new PdfPCell(new Phrase("STOCK", fuenteTotales));
+            colStock.BorderWidth = 0;
+            colStock.HorizontalAlignment = Element.ALIGN_CENTER;
+            colStock.Padding = 3;
+            colStock.BackgroundColor = new BaseColor(Color.SkyBlue);
+
+            PdfPCell colPrecio = new PdfPCell(new Phrase("PRECIO", fuenteTotales));
+            colPrecio.BorderWidth = 0;
+            colPrecio.HorizontalAlignment = Element.ALIGN_CENTER;
+            colPrecio.Padding = 3;
+            colPrecio.BackgroundColor = new BaseColor(Color.SkyBlue);
+
+            tablaInventario.AddCell(colNoConcepto);
+            tablaInventario.AddCell(colNombre);
+            tablaInventario.AddCell(colVendidos);
+            tablaInventario.AddCell(colCodigoBarra);
+            tablaInventario.AddCell(colCategoria);
+            tablaInventario.AddCell(colUltimaVenta);
+            tablaInventario.AddCell(colStock);
+            tablaInventario.AddCell(colPrecio);
+
+            for (int i = 0; i < cantidadLimite; i++)
+            {
+                var articulo = dtProductosMenosVendidos.Rows[i]["ARTICULO"].ToString();
+                var vendidos = dtProductosMenosVendidos.Rows[i]["VENDIDOS"].ToString();
+                var codigoBarra = dtProductosMenosVendidos.Rows[i]["CODIGO DE BARRAS"].ToString();
+                var categoria = dtProductosMenosVendidos.Rows[i]["CATEGORIA"].ToString();
+                var ultimaVenta = dtProductosMenosVendidos.Rows[i]["ULTIMA VENTA"].ToString();
+                var stock = dtProductosMenosVendidos.Rows[i]["STOCK"].ToString();
+                var precio = dtProductosMenosVendidos.Rows[i]["PRECIO"].ToString();
+
+                numRow++;
+
+                PdfPCell colNoConceptoTmp = new PdfPCell(new Phrase(numRow.ToString("N0"), fuenteNormal));
+                colNoConceptoTmp.BorderWidth = 1;
+                colNoConceptoTmp.HorizontalAlignment = Element.ALIGN_LEFT;
+
+                PdfPCell colNombreTmp = new PdfPCell(new Phrase(articulo, fuenteNormal));
+                colNombreTmp.BorderWidth = 1;
+                colNombreTmp.HorizontalAlignment = Element.ALIGN_LEFT;
+
+                PdfPCell colVendidosTmp = new PdfPCell(new Phrase(vendidos, fuenteNormal));
+                colVendidosTmp.BorderWidth = 1;
+                colVendidosTmp.HorizontalAlignment = Element.ALIGN_CENTER;
+
+                PdfPCell colCodigoBarraTmp = new PdfPCell(new Phrase(codigoBarra, fuenteNormal));
+                colCodigoBarraTmp.BorderWidth = 1;
+                colCodigoBarraTmp.HorizontalAlignment = Element.ALIGN_CENTER;
+
+                PdfPCell colCategoriaTmp = new PdfPCell(new Phrase(categoria, fuenteNormal));
+                colCategoriaTmp.BorderWidth = 1;
+                colCategoriaTmp.HorizontalAlignment = Element.ALIGN_CENTER;
+
+                PdfPCell colUltimaVentaTmp = new PdfPCell(new Phrase(ultimaVenta, fuenteNormal));
+                colUltimaVentaTmp.BorderWidth = 1;
+                colUltimaVentaTmp.HorizontalAlignment = Element.ALIGN_CENTER;
+
+                PdfPCell colStockTmp = new PdfPCell(new Phrase(stock, fuenteNormal));
+                colStockTmp.BorderWidth = 1;
+                colStockTmp.HorizontalAlignment = Element.ALIGN_CENTER;
+
+                PdfPCell colPrecioTmp = new PdfPCell(new Phrase(precio, fuenteNormal));
+                colPrecioTmp.BorderWidth = 1;
+                colPrecioTmp.HorizontalAlignment = Element.ALIGN_CENTER;
+
+                tablaInventario.AddCell(colNoConceptoTmp);
+                tablaInventario.AddCell(colNombreTmp);
+                tablaInventario.AddCell(colVendidosTmp);
+                tablaInventario.AddCell(colCodigoBarraTmp);
+                tablaInventario.AddCell(colCategoriaTmp);
+                tablaInventario.AddCell(colUltimaVentaTmp);
+                tablaInventario.AddCell(colStockTmp);
+                tablaInventario.AddCell(colPrecioTmp);
+            }
+
+            reporte.Add(titulo);
+            reporte.Add(Usuario);
+            reporte.Add(subTitulo);
+            reporte.Add(tablaInventario);
+
+            reporte.AddTitle("Reporte Producto Menos Vendido");
+            reporte.AddAuthor("PUDVE");
+            reporte.Close();
+            writer.Close();
+
+            dtProductosMenosVendidos.Dispose();
+            dtProductosMenosVendidos = null;
+
+            VisualizadorReportes vr = new VisualizadorReportes(rutaArchivo);
+            vr.ShowDialog();
         }
     }
 }
