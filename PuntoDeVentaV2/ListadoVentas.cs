7f7ec4410ea60706684f3b6a3b2e6b79702555d7
Emmanuel Ruiz García <emmanuel.ruiz.garcia@outlook.com>
2022-02-10 15:34:53 -0600

    Como Empleado Podre Ver Todas Las Ventas A Crédito Que Se Realizaron En El Sistema Al Dar Click En El Botón Buscar Modificando La Búsqueda, La Primer Y Segunda Fecha

--- PuntoDeVentaV2/ListadoVentas.cs
+++ PuntoDeVentaV2/ListadoVentas.cs
@@ -369,7 +369,7 @@ namespace PuntoDeVentaV2
                             }
                             else if (estado.Equals(4)) // Ventas a credito
                             {
-                                consulta = cs.VerComoEmpleadoTodasLasVentasACreditoPorFechasYBusqueda(estado, fechaInicial, fechaFinal, extra); 
+                                consulta = cs.VerComoEmpleadoTodasLasVentasACreditoPorFechasYBusqueda(estado, fechaInicial, fechaFinal, extra);
                             }
                         }
                         else
