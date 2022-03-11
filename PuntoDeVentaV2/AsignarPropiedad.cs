c1ac1d3ff9363f4d378d7629ab28000042c404c0
AlonzoJimenezI <alonso_2nov@hotmail.com>
2022-03-11 12:47:31 -0600

    Se cambiaron el texto de los mensajes de asignar

--- PuntoDeVentaV2/AsignarPropiedad.cs
+++ PuntoDeVentaV2/AsignarPropiedad.cs
@@ -1060,7 +1060,7 @@ namespace PuntoDeVentaV2
                             }
                         }
                     }
-                    MessageBox.Show("Asignacion con exito","Mensajes del sistema",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                    MessageBox.Show("ASIGNACION MULTIPLE REALIZADA CON EXITO", "Mensajes del sistema",MessageBoxButtons.OK,MessageBoxIcon.Information);
                 }
                 else
                 {
@@ -1152,7 +1152,7 @@ namespace PuntoDeVentaV2
                             }
                         }
                     }
-                    MessageBox.Show("Asignacion con exito", "Mensajes del sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("ASIGNACION MULTIPLE REALIZADA CON EXITO", "Mensajes del sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
                 
             }
@@ -1237,7 +1237,7 @@ namespace PuntoDeVentaV2
 
                         notificacion.Start();
                     }
-                    MessageBox.Show("Asignacion con exito", "Mensajes del sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("ASIGNACION MULTIPLE REALIZADA CON EXITO", "Mensajes del sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
                 else
                 {
@@ -1273,7 +1273,7 @@ namespace PuntoDeVentaV2
 
                         cn.EjecutarConsulta(consulta);
                     }
-                    MessageBox.Show("Asignacion con exito", "Mensajes del sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("ASIGNACION MULTIPLE REALIZADA CON EXITO", "Mensajes del sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
                 else
                 {
@@ -1309,7 +1309,7 @@ namespace PuntoDeVentaV2
 
                         cn.EjecutarConsulta(consulta);
                     }
-                    MessageBox.Show("Asignacion con exito", "Mensajes del sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("ASIGNACION MULTIPLE REALIZADA CON EXITO", "Mensajes del sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
                 else
                 {
@@ -1415,7 +1415,7 @@ namespace PuntoDeVentaV2
 
                         notificacion.Start();
                     }
-                    MessageBox.Show("Asignacion con exito", "Mensajes del sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("ASIGNACION MULTIPLE REALIZADA CON EXIT", "Mensajes del sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
                 else
                 {
@@ -1449,7 +1449,7 @@ namespace PuntoDeVentaV2
 
                         cn.EjecutarConsulta(consulta);
                     }
-                    MessageBox.Show("Asignacion con exito", "Mensajes del sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("ASIGNACION MULTIPLE REALIZADA CON EXITO", "Mensajes del sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
                 else
                 {
@@ -1479,6 +1479,7 @@ namespace PuntoDeVentaV2
                     consulta += valores + " ON DUPLICATE KEY UPDATE ID = VALUES(ID), Impuesto = VALUES(Impuesto);";
 
                     cn.EjecutarConsulta(consulta);
+                    MessageBox.Show("ASIGNACION MULTIPLE REALIZADA CON EXITO", "Mensajes del sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
             }
             else if (propiedad == "ClaveProducto")
@@ -1505,7 +1506,7 @@ namespace PuntoDeVentaV2
 
                         cn.EjecutarConsulta(consulta);
                     }
-                    MessageBox.Show("Asignacion con exito", "Mensajes del sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("ASIGNACION MULTIPLE REALIZADA CON EXITO", "Mensajes del sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
                 else
                 {
@@ -1539,7 +1540,7 @@ namespace PuntoDeVentaV2
 
                         cn.EjecutarConsulta(consulta);
                     }
-                    MessageBox.Show("Asignacion con exito", "Mensajes del sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("ASIGNACION MULTIPLE REALIZADA CON EXITO", "Mensajes del sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
                 else
                 {
@@ -1584,7 +1585,7 @@ namespace PuntoDeVentaV2
                     consulta += valores + " ON DUPLICATE KEY UPDATE ID = VALUES(ID), IDUsuario = VALUES(IDUsuario), IDProducto = VALUES(IDProducto), CorreoVentaProducto = VALUES(CorreoVentaProducto);";
 
                     cn.EjecutarConsulta(consulta);
-                    MessageBox.Show("Asignacion con exito", "Mensajes del sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("ASIGNACION MULTIPLE REALIZADA CON EXITO", "Mensajes del sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
             }
             else if (propiedad == "Proveedor")
@@ -1622,7 +1623,7 @@ namespace PuntoDeVentaV2
 
                         cn.EjecutarConsulta(consulta);
                     }
-                    MessageBox.Show("Asignacion con exito", "Mensajes del sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("ASIGNACION MULTIPLE REALIZADA CON EXITO", "Mensajes del sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
                 else
                 {
@@ -1674,7 +1675,7 @@ namespace PuntoDeVentaV2
                     consulta += valores + " ON DUPLICATE KEY UPDATE ID = VALUES(ID), IDProducto = VALUES(IDProducto), IDUsuario = VALUES(IDUsuario), IDDetalleGral = VALUES(IDDetalleGral), StatusDetalleGral = VALUES(StatusDetalleGral), panelContenido = VALUES(panelContenido);";
 
                     cn.EjecutarConsulta(consulta);
-                    MessageBox.Show("Asignacion con exito", "Mensajes del sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("ASIGNACION MULTIPLE REALIZADA CON EXITO", "Mensajes del sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
             }
         }
