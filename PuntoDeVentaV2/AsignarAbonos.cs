023ed73de895e9d96e46982eb05c35591c048a37
AndoniSG14 <laletrabeto@gmail.com>
2022-04-11 11:49:02 -0500

    Se corrigio el error de que si una pago acredito que paga a varios abonos, al llegar a 0 lo borre y agrege a ventas pagadas

--- PuntoDeVentaV2/AsignarAbonos.cs
+++ PuntoDeVentaV2/AsignarAbonos.cs
@@ -135,6 +135,11 @@ namespace PuntoDeVentaV2
                     cn.EjecutarConsulta(cs.ActualizarVenta(idVenta, 1, FormPrincipal.userID));
                 }
 
+                if (restante <= 0)
+                {
+                    cn.EjecutarConsulta(cs.estatusFinalizacionPagoCredito(idVenta));
+                }
+
                 //var pagoPendiente = txtPendiente.Text;
                 //var cantidadPendiente = float.Parse(pagoPendiente);
                 //var operacionTotal = (total - totalPendiente);
