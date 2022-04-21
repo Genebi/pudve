9cdb4978447c06d6d478d532e0dc691eba01997b
AlonzoJimenezI <alonso_2nov@hotmail.com>
2022-04-21 14:42:58 -0500

    Se corrigio detalle al cancelar una venta desde un empleado no se reflejaba en Caja

--- PuntoDeVentaV2/ListadoVentas.cs
+++ PuntoDeVentaV2/ListadoVentas.cs
@@ -1676,7 +1676,7 @@ namespace PuntoDeVentaV2
 
                                             string[] datos = new string[] {
                                                         "retiro", total1, "0", conceptoCredito, fechaOperacion1, FormPrincipal.userID.ToString(),
-                                                        efectivo1, tarjeta1, vales1, cheque1, transferencia1, credito1/*"0.00"*/, /*anticipo*/"0"
+                                                        efectivo1, tarjeta1, vales1, cheque1, transferencia1, credito1/*"0.00"*/, /*anticipo*/"0", FormPrincipal.id_empleado.ToString()
                                                     };
                                             cn.EjecutarConsulta(cs.OperacionCaja(datos));
                                         }
@@ -1840,7 +1840,7 @@ namespace PuntoDeVentaV2
                                             {
                                                 string[] datos = new string[] {
                                                         "retiro", total1, "0", conceptoCredito, fechaOperacion1, FormPrincipal.userID.ToString(),
-                                                        efectivo1, tarjeta1, vales1, cheque1, transferencia1, credito1/*"0.00"*/, /*anticipo*/"0"
+                                                        efectivo1, tarjeta1, vales1, cheque1, transferencia1, credito1/*"0.00"*/, /*anticipo*/"0",FormPrincipal.id_empleado.ToString()
                                                     };
                                                 cn.EjecutarConsulta(cs.OperacionCaja(datos));
                                                 //string[] datos = new string[]
@@ -2013,7 +2013,7 @@ namespace PuntoDeVentaV2
                                             {
                                                 string[] datos = new string[] {
                                                         "retiro", total1, "0", conceptoCreditoC, fechaOperacion1, FormPrincipal.userID.ToString(),
-                                                        efectivo1, tarjeta1, vales1, cheque1, transferencia1, credito1/*"0.00"*/, /*anticipo*/"0"
+                                                        efectivo1, tarjeta1, vales1, cheque1, transferencia1, credito1/*"0.00"*/, /*anticipo*/"0",FormPrincipal.id_empleado.ToString()
                                                     };
                                                 cn.EjecutarConsulta(cs.OperacionCaja(datos));
                                             }
