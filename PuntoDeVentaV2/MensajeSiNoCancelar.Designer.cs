580f84b2a3c13bab928db54ad0c3232a779b18ef
AndoniSG14 <laletrabeto@gmail.com>
2022-06-24 09:49:09 -0500

    Se cambio el diseño de la ventana de de mensajeSiNoCancelar

--- PuntoDeVentaV2/MensajeSiNoCancelar.Designer.cs
+++ PuntoDeVentaV2/MensajeSiNoCancelar.Designer.cs
@@ -29,17 +29,19 @@ namespace PuntoDeVentaV2
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MensajeSiNoCancelar));
             this.btnSi = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.btnNo = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnSi
             // 
             this.btnSi.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSi.Location = new System.Drawing.Point(31, 70);
+            this.btnSi.Location = new System.Drawing.Point(77, 80);
             this.btnSi.Name = "btnSi";
-            this.btnSi.Size = new System.Drawing.Size(118, 35);
+            this.btnSi.Size = new System.Drawing.Size(67, 30);
             this.btnSi.TabIndex = 0;
             this.btnSi.Text = "Si";
             this.btnSi.UseVisualStyleBackColor = true;
@@ -48,35 +50,44 @@ namespace PuntoDeVentaV2
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(40, 28);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(78, 43);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(244, 20);
+            this.label1.Size = new System.Drawing.Size(240, 18);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Desea Guardar los Cambios?";
+            this.label1.Text = "¿Desea Guardar los Cambios?";
             // 
             // btnNo
             // 
             this.btnNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnNo.Location = new System.Drawing.Point(178, 70);
+            this.btnNo.Location = new System.Drawing.Point(212, 80);
             this.btnNo.Name = "btnNo";
-            this.btnNo.Size = new System.Drawing.Size(118, 35);
+            this.btnNo.Size = new System.Drawing.Size(67, 30);
             this.btnNo.TabIndex = 3;
             this.btnNo.Text = "No";
             this.btnNo.UseVisualStyleBackColor = true;
             this.btnNo.Click += new System.EventHandler(this.btnNo_Click);
             // 
+            // label2
+            // 
+            this.label2.Image = ((System.Drawing.Image)(resources.GetObject("label2.Image")));
+            this.label2.Location = new System.Drawing.Point(31, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 42);
+            this.label2.TabIndex = 4;
+            // 
             // MensajeSiNoCancelar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(327, 127);
+            this.ClientSize = new System.Drawing.Size(351, 127);
             this.Controls.Add(this.btnNo);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnSi);
+            this.Controls.Add(this.label2);
             this.Name = "MensajeSiNoCancelar";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Aviso del Sistema!";
+            this.Text = "!Aviso del Sistema!";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -87,5 +98,6 @@ namespace PuntoDeVentaV2
         private System.Windows.Forms.Button btnSi;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnNo;
+        private System.Windows.Forms.Label label2;
     }
 }
