e001e3bd66874522acfd04db85107ada2369622e
Emmanuel Ruiz Garcia <emmanuel.ruiz.garcia@outlook.com>
2022-07-07 16:45:02 -0500

    se agrego funcionalidad de imprimir ticket de dinero agregado desde Administrador falta en empleado

--- PuntoDeVentaV2/ImprimirTicketDepositarDineroCaja8cm.cs
+++ PuntoDeVentaV2/ImprimirTicketDepositarDineroCaja8cm.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Microsoft.Reporting.WinForms;
+using MySql.Data.MySqlClient;
+using PuntoDeVentaV2.ReportesImpresion;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +16,9 @@ namespace PuntoDeVentaV2
 {
     public partial class ImprimirTicketDepositarDineroCaja8cm : Form
     {
+        Consultas cs = new Consultas();
+        Conexion cn = new Conexion();
+
         public int idDineroAgregado { get; set; }
 
         public ImprimirTicketDepositarDineroCaja8cm()
@@ -21,7 +28,94 @@ namespace PuntoDeVentaV2
 
         private void ImprimirTicketDepositarDineroCaja8cm_Load(object sender, EventArgs e)
         {
+            CargarDatosTicket();
+        }
 
+        private void CargarDatosTicket()
+        {
+            var servidor = Properties.Settings.Default.Hosting;
+            string cadenaConn = string.Empty;
+            string queryDineroAgregado = cs.obtenerDatosTicketAgregarDinero(idDineroAgregado);
+            MySqlConnection conn = new MySqlConnection();
+            string pathApplication = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string FullReportPath = $@"{pathApplication}\ReportesImpresion\Ticket\DineroAgregado\TicketDineroAgregado.rdlc";
+            string DirectoryImage = string.Empty;
+            string path = string.Empty;
+            string pathBarCode = $@"C:\Archivos PUDVE\Ventas\Tickets\BarCode\";
+            var folioVentaRealizada = string.Empty;
+            var finalLogoTipoPath = string.Empty;
+
+            if (!string.IsNullOrWhiteSpace(servidor))
+            {
+                cadenaConn = $"datasource={servidor};port=6666;username=root;password=;database=pudve;";
+            }
+            else
+            {
+                cadenaConn = "datasource=127.0.0.1;port=6666;username=root;password=;database=pudve";
+            }
+
+            conn.ConnectionString = cadenaConn;
+
+            try
+            {
+                conn.Open();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Aviso del Sistema", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            MySqlDataAdapter dineroAgregadoDA = new MySqlDataAdapter(queryDineroAgregado, conn);
+            DataTable dineroAgregadoDT = new DataTable();
+
+            dineroAgregadoDA.Fill(dineroAgregadoDT);
+
+            this.reportViewer1.ProcessingMode = ProcessingMode.Local;
+            this.reportViewer1.LocalReport.ReportPath = FullReportPath;
+            this.reportViewer1.LocalReport.DataSources.Clear();
+
+            #region Impresion Ticket de 8 cm (80 mm)
+            ReportDataSource rp = new ReportDataSource("TicketDineroAgregado", dineroAgregadoDT);
+
+            this.reportViewer1.LocalReport.DataSources.Add(rp);
+            this.reportViewer1.ZoomMode = ZoomMode.PageWidth;
+            this.reportViewer1.RefreshReport();
+
+            LocalReport rdlc = new LocalReport();
+            rdlc.ReportPath = FullReportPath;
+            rdlc.DataSources.Add(rp);
+            #endregion
+
+            EnviarImprimir imp = new EnviarImprimir();
+            imp.Imprime(rdlc);
+
+            this.Close();
+        }
+
+        private Image GenerarCodigoBarras(string txtCodigo, int ancho)
+        {
+            Image imagen;
+
+            BarcodeLib.Barcode codigo = new BarcodeLib.Barcode();
+
+            try
+            {
+                var anchoTmp = ancho / 2;
+                var auxiliar = anchoTmp;
+
+                anchoTmp = auxiliar / 2;
+                ancho = ancho - anchoTmp;
+
+                imagen = codigo.Encode(BarcodeLib.TYPE.CODE128, txtCodigo, Color.Black, Color.White, ancho, 40);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al generar código de barras para el ticket", "Mensaje del Sistema", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                imagen = null;
+            }
+
+            return imagen;
         }
     }
 }
