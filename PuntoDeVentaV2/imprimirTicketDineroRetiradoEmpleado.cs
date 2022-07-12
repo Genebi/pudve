8c3a7e4ca6b6a9a99201dc167f6ffbb6abe6a43c
Emmanuel Ruiz Garcia <emmanuel.ruiz.garcia@outlook.com>
2022-07-12 09:51:51 -0500

    se implemento el retiro de dinero desde empleado en la sección de caja

---
+++ PuntoDeVentaV2/imprimirTicketDineroRetiradoEmpleado.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Reporting.WinForms;
+using MySql.Data.MySqlClient;
+using PuntoDeVentaV2.ReportesImpresion;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PuntoDeVentaV2
+{
+    public partial class imprimirTicketDineroRetiradoEmpleado : Form
+    {
+        Consultas cs = new Consultas();
+        Conexion cn = new Conexion();
+
+        public int idDineroRetirado { get; set; }
+
+        public imprimirTicketDineroRetiradoEmpleado()
+        {
+            InitializeComponent();
+        }
+
+        private void imprimirTicketDineroRetiradoEmpleado_Load(object sender, EventArgs e)
+        {
+            CargarDatosTicket();
+        }
+
+        private void CargarDatosTicket()
+        {
+            var servidor = Properties.Settings.Default.Hosting;
+            string cadenaConn = string.Empty;
+            string queryDineroRetirado = cs.obtenerDatosTicketRetirarDineroEmpleado(idDineroRetirado);
+            MySqlConnection conn = new MySqlConnection();
+            string pathApplication = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string FullReportPath = $@"{pathApplication}\ReportesImpresion\Ticket\DineroRetirado\TicketDineroRetirado.rdlc";
+            string DirectoryImage = string.Empty;
+            string path = string.Empty;
+            string pathBarCode = $@"C:\Archivos PUDVE\Ventas\Tickets\BarCode\";
+            var folioVentaRealizada = string.Empty;
+            var finalLogoTipoPath = string.Empty;
+
+            if (!string.IsNullOrWhiteSpace(servidor))
+            {
+                cadenaConn = $"datasource={servidor};port=6666;username=root;password=;database=pudve;";
+            }
+            else
+            {
+                cadenaConn = "datasource=127.0.0.1;port=6666;username=root;password=;database=pudve";
+            }
+
+            conn.ConnectionString = cadenaConn;
+
+            try
+            {
+                conn.Open();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Aviso del Sistema", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            MySqlDataAdapter dineroRetiradoDA = new MySqlDataAdapter(queryDineroRetirado, conn);
+            DataTable dineroRetiradoDT = new DataTable();
+
+            dineroRetiradoDA.Fill(dineroRetiradoDT);
+
+            this.reportViewer1.ProcessingMode = ProcessingMode.Local;
+            this.reportViewer1.LocalReport.ReportPath = FullReportPath;
+            this.reportViewer1.LocalReport.DataSources.Clear();
+
+            #region Impresion Ticket de 8 cm (80 mm)
+            ReportDataSource rp = new ReportDataSource("TicketDineroRetirado", dineroRetiradoDT);
+
+            this.reportViewer1.LocalReport.DataSources.Add(rp);
+            this.reportViewer1.ZoomMode = ZoomMode.PageWidth;
+            this.reportViewer1.RefreshReport();
+
+            LocalReport rdlc = new LocalReport();
+            rdlc.ReportPath = FullReportPath;
+            rdlc.DataSources.Add(rp);
+            #endregion
+
+            EnviarImprimir imp = new EnviarImprimir();
+            imp.Imprime(rdlc);
+
+            this.Close();
+        }
+    }
+}
