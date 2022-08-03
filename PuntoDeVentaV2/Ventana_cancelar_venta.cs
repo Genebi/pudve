e505773ad0cff80c49927ec1346feed4b4d33703
Emmanuel Ruiz Garcia <emmanuel.ruiz.garcia@outlook.com>
2022-08-03 11:44:02 -0500

    modificación de consulta de SQL para traer el concepto de referencia

--- PuntoDeVentaV2/Ventana_cancelar_venta.cs
+++ PuntoDeVentaV2/Ventana_cancelar_venta.cs
@@ -267,6 +267,7 @@ namespace PuntoDeVentaV2
                                             var ticket8cm = 0;
                                             var ticket6cm = 0;
                                             var codigoBarraTicket = 0;
+                                            var referencia = 0;
 
                                             foreach (DataRow item in dtConfiguracionTipoTicket.Rows)
                                             {
@@ -288,6 +289,7 @@ namespace PuntoDeVentaV2
                                                 ticket6cm = Convert.ToInt32(item["ticket58mm"].ToString());
                                                 ticket8cm = Convert.ToInt32(item["ticket80mm"].ToString());
                                                 codigoBarraTicket = Convert.ToInt32(item["TicketVenta"].ToString());
+                                                referencia = Convert.ToInt32(item["Referencia"].ToString());
                                             }
 
                                             DialogResult respuestaImpresion = MessageBox.Show("Desea Imprimir El Ticket De La Cancelación", "Aviso", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
