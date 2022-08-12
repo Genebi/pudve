6705b80645b0a7dcaa93e54b04b6eae8c2b16e0b
AlonsoJimenezI <Alonso_2nov@hotmail.com>
2022-08-12 10:23:33 -0500

    Mafufada david de otro pdf de ventas v2

---
+++ PuntoDeVentaV2/FormNotaVentaHDA.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace PuntoDeVentaV2
+{
+    partial class FormNotaVentaHDA
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImprimir = new PuntoDeVentaV2.BotonRedondo();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.SuspendLayout();
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnImprimir.BackGroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnImprimir.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnImprimir.BorderRadius = 20;
+            this.btnImprimir.BorderSize = 0;
+            this.btnImprimir.FlatAppearance.BorderSize = 0;
+            this.btnImprimir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnImprimir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImprimir.ForeColor = System.Drawing.Color.White;
+            this.btnImprimir.Location = new System.Drawing.Point(348, 470);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(150, 40);
+            this.btnImprimir.TabIndex = 3;
+            this.btnImprimir.Text = "Imprimir";
+            this.btnImprimir.TextColor = System.Drawing.Color.White;
+            this.btnImprimir.UseVisualStyleBackColor = false;
+            this.btnImprimir.Click += new System.EventHandler(this.btnImprimir_Click);
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(864, 467);
+            this.reportViewer1.TabIndex = 2;
+            // 
+            // FormNotaVentaHDA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 511);
+            this.Controls.Add(this.btnImprimir);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "FormNotaVentaHDA";
+            this.Text = "FormNotaVentaHDA";
+            this.Load += new System.EventHandler(this.FormNotaVentaHDA_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private BotonRedondo btnImprimir;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+    }
+}
---
+++ PuntoDeVentaV2/FormNotaVentaHDA.cs
@@ -0,0 +1,281 @@
+﻿using Microsoft.Reporting.WinForms;
+using MySql.Data.MySqlClient;
+using PuntoDeVentaV2.ReportesImpresion;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PuntoDeVentaV2
+{
+    public partial class FormNotaVentaHDA : Form
+    {
+
+        Consultas cs = new Consultas();
+        Conexion cn = new Conexion();
+        Moneda oMoneda = new Moneda();
+        int IDVenta;
+        decimal Total = 0;
+        string resultado;
+        public static string ruta_archivos_guadados = @"C:\Archivos PUDVE\MisDatos\CSD\";
+        string[] usuario;
+        string nombreUsuario;
+        string DireccionLogo;
+        bool SiHayLogo = false;
+        string pathLogoImage;
+        public FormNotaVentaHDA(int IDDeLaVEnta)
+        {
+            InitializeComponent();
+            this.IDVenta = IDDeLaVEnta;
+        }
+
+        private void btnImprimir_Click(object sender, EventArgs e)
+        {
+            string cadenaConn = string.Empty;
+            string queryVentas = string.Empty;
+
+
+            if (!string.IsNullOrWhiteSpace(Properties.Settings.Default.Hosting))
+            {
+                cadenaConn = $"datasource={Properties.Settings.Default.Hosting};port=6666;username=root;password=;database=pudve;";
+            }
+            else
+            {
+                cadenaConn = "datasource=127.0.0.1;port=6666;username=root;password=;database=pudve;";
+            }
+
+            queryVentas = cs.PDFNotaDeVentasHDA(IDVenta);
+
+
+
+            MySqlConnection conn = new MySqlConnection();
+
+            conn.ConnectionString = cadenaConn;
+
+            try
+            {
+                conn.Open();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+
+            string pathApplication = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string FullReportPath = $@"{pathApplication}\ReportesImpresion\Ticket\NotasVentasHDA\NotaVentaHDA.rdlc";
+
+            //imagen
+
+            var servidor = Properties.Settings.Default.Hosting;
+            string saveDirectoryImg = @"C:\Archivos PUDVE\MisDatos\Usuarios\";
+
+            using (DataTable ConsultaLogo = cn.CargarDatos(cs.buscarNombreLogoTipo2(FormPrincipal.userID)))
+            {
+                if (!ConsultaLogo.Rows.Count.Equals(0))
+                {
+                    string Logo = ConsultaLogo.Rows[0]["Logo"].ToString();
+                    if (!Logo.Equals(""))
+                    {
+
+                        if (!Directory.Exists(saveDirectoryImg))    // verificamos que si no existe el directorio
+                        {
+                            Directory.CreateDirectory(saveDirectoryImg);    // lo crea para poder almacenar la imagen
+                        }
+                        if (!string.IsNullOrWhiteSpace(servidor))
+                        {
+                            // direccion de la carpeta donde se va poner las imagenes
+                            pathLogoImage = new Uri($"C:/Archivos PUDVE/MisDatos/Usuarios/").AbsoluteUri;
+                            // ruta donde estan guardados los archivos digitales
+                            ruta_archivos_guadados = $@"\\{servidor}\Archivos PUDVE\MisDatos\CSD_{Logo}\";
+                        }
+                        else
+                        {
+                            // direccion de la carpeta donde se va poner las imagenes
+                            pathLogoImage = new Uri($"C:/Archivos PUDVE/MisDatos/Usuarios/").AbsoluteUri;
+                            // ruta donde estan guardados los archivos digitales
+                            ruta_archivos_guadados = $@"C:\Archivos PUDVE\MisDatos\CSD_{Logo}\";
+
+                            DireccionLogo = pathLogoImage + Logo;
+
+                        }
+                        SiHayLogo = true;
+                    }
+                }
+            }
+            //imagen
+
+            MySqlDataAdapter retiroDA = new MySqlDataAdapter(queryVentas, conn);
+            DataTable DTNotaDeVentas = new DataTable();
+            retiroDA.Fill(DTNotaDeVentas);
+
+            Total = Convert.ToDecimal(DTNotaDeVentas.Rows[0]["total"]);
+            resultado = oMoneda.Convertir(Total.ToString(), true, "PESOS");
+
+            ReportParameterCollection reportParameters = new ReportParameterCollection();
+            reportParameters.Add(new ReportParameter("TotalEnTexto", resultado));
+            if (SiHayLogo.Equals(true))
+            {
+                reportParameters.Add(new ReportParameter("Logo", DireccionLogo));
+            }
+            else
+            {
+                DireccionLogo = "";
+                reportParameters.Add(new ReportParameter("Logo", DireccionLogo));
+            }
+            reportParameters.Add(new ReportParameter("StatusVenta", ""));
+
+            ReportDataSource NotasVENTAS = new ReportDataSource("DTNotaVenta", DTNotaDeVentas);
+
+            LocalReport rdlc = new LocalReport();
+            rdlc.ReportPath = FullReportPath;
+            rdlc.EnableExternalImages = true;
+            rdlc.DataSources.Add(NotasVENTAS);
+            rdlc.SetParameters(reportParameters);
+
+            FormatoCarta imp = new FormatoCarta();
+            imp.Imprime(rdlc);
+
+            this.Close();
+        }
+
+        private void FormNotaVentaHDA_Load(object sender, EventArgs e)
+        {
+            CargarNotaDeVenta();
+        }
+
+        private void CargarNotaDeVenta()
+        {
+            string cadenaConn = string.Empty;
+            string queryVentas = string.Empty;
+
+
+            if (!string.IsNullOrWhiteSpace(Properties.Settings.Default.Hosting))
+            {
+                cadenaConn = $"datasource={Properties.Settings.Default.Hosting};port=6666;username=root;password=;database=pudve;";
+            }
+            else
+            {
+                cadenaConn = "datasource=127.0.0.1;port=6666;username=root;password=;database=pudve;";
+            }
+
+            queryVentas = cs.PDFNotaDeVentasHDA(IDVenta);
+
+
+             
+            MySqlConnection conn = new MySqlConnection();
+
+            conn.ConnectionString = cadenaConn;
+
+            try
+            {
+                conn.Open();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+
+            string pathApplication = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string FullReportPath = $@"{pathApplication}\ReportesImpresion\Ticket\NotasVentasHDA\NotaVentaHDA.rdlc";
+
+            //imagen
+
+            var servidor = Properties.Settings.Default.Hosting;
+            string saveDirectoryImg = @"C:\Archivos PUDVE\MisDatos\Usuarios\";
+
+            using (DataTable ConsultaLogo = cn.CargarDatos(cs.buscarNombreLogoTipo2(FormPrincipal.userID)))
+            {
+                if (!ConsultaLogo.Rows.Count.Equals(0))
+                {
+                    string Logo = ConsultaLogo.Rows[0]["Logo"].ToString();
+                    if (!Logo.Equals(""))
+                    {
+
+                        if (!Directory.Exists(saveDirectoryImg))    // verificamos que si no existe el directorio
+                        {
+                            Directory.CreateDirectory(saveDirectoryImg);    // lo crea para poder almacenar la imagen
+                        }
+                        if (!string.IsNullOrWhiteSpace(servidor))
+                        {
+                            // direccion de la carpeta donde se va poner las imagenes
+                            pathLogoImage = new Uri($"C:/Archivos PUDVE/MisDatos/Usuarios/").AbsoluteUri;
+                            // ruta donde estan guardados los archivos digitales
+                            ruta_archivos_guadados = $@"\\{servidor}\Archivos PUDVE\MisDatos\CSD_{Logo}\";
+                        }
+                        else
+                        {
+                            // direccion de la carpeta donde se va poner las imagenes
+                            pathLogoImage = new Uri($"C:/Archivos PUDVE/MisDatos/Usuarios/").AbsoluteUri;
+                            // ruta donde estan guardados los archivos digitales
+                            ruta_archivos_guadados = $@"C:\Archivos PUDVE\MisDatos\CSD_{Logo}\";
+
+                            DireccionLogo = pathLogoImage + Logo;
+
+                        }
+                        SiHayLogo = true;
+                    }
+                }
+            }
+            //imagen
+
+            MySqlDataAdapter retiroDA = new MySqlDataAdapter(queryVentas, conn);
+            DataTable DTNotaDeVentas = new DataTable();
+            retiroDA.Fill(DTNotaDeVentas);
+
+            Total = Convert.ToDecimal(DTNotaDeVentas.Rows[0]["total"]);
+            resultado = oMoneda.Convertir(Total.ToString(), true, "PESOS");
+
+            ReportParameterCollection reportParameters = new ReportParameterCollection();
+            reportParameters.Add(new ReportParameter("TotalEnTexto", resultado));
+            if (SiHayLogo.Equals(true))
+            {
+                reportParameters.Add(new ReportParameter("Logo", DireccionLogo));
+            }
+            else
+            {
+                DireccionLogo = "";
+                reportParameters.Add(new ReportParameter("Logo", DireccionLogo));
+            }
+            string StatusVenta;
+            using (DataTable ConsultaEstatus = cn.CargarDatos($"SELECT `Status` FROM ventas WHERE ID = {IDVenta}"))
+            {
+                string Status = ConsultaEstatus.Rows[0]["Status"].ToString();
+                if (Status.Equals("1"))
+                {
+                    StatusVenta = "Venta Pagada";
+                }
+                else if (Status.Equals("2"))
+                {
+                    StatusVenta = "Presupuesto";
+                }
+                else if (Status.Equals("3"))
+                {
+                    StatusVenta = "Venta Cancelada";
+                }
+                else
+                {
+                    StatusVenta = "Venta a Crédito";
+                }
+            }
+            reportParameters.Add(new ReportParameter("StatusVenta", StatusVenta));
+
+            this.reportViewer1.ProcessingMode = ProcessingMode.Local;
+            this.reportViewer1.LocalReport.ReportPath = FullReportPath;
+            this.reportViewer1.LocalReport.DataSources.Clear();
+
+            ReportDataSource NotasVENTAS = new ReportDataSource("DTNotaVentaHDA", DTNotaDeVentas);
+
+            this.reportViewer1.ZoomMode = ZoomMode.PageWidth;
+            this.reportViewer1.LocalReport.DataSources.Add(NotasVENTAS);
+            this.reportViewer1.LocalReport.EnableExternalImages = true;
+            this.reportViewer1.LocalReport.SetParameters(reportParameters);
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
