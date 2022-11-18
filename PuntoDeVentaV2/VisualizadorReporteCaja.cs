8edde9505b005d779bbe20ea8d647f2bd15e2602
Alejandro <genebi@outlook.com>
2022-11-17 20:05:09 -0600

    Reporte estilos III

--- PuntoDeVentaV2/VisualizadorReporteCaja.cs
+++ PuntoDeVentaV2/VisualizadorReporteCaja.cs
@@ -66,8 +66,16 @@ namespace PuntoDeVentaV2
             string pathApplication = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
             string FullReportPath = $@"{pathApplication}\ReportesImpresion\Ticket\ImpimirReporteCaja\ReporteCaja.rdlc";
 
+
+            MySqlDataAdapter reporteCajaDA = new MySqlDataAdapter(queryReporteCaja, conn);
+            DataTable reporteCajaDT = new DataTable();
+
+            reporteCajaDA.Fill(reporteCajaDT);
+
             //========================================================================
-            // DATOS PARA EL TITULO DEL REPORTE
+            // DATOS PARA EL TITULO DEL REPORTE Y CANTIDADES TOTALES
+            //========================================================================
+
             var usuarioActivo = cs.validarEmpleado(FormPrincipal.userNickName, true);
             var obtenerUsuarioPrincipal = cs.validarEmpleadoPorID();
 
@@ -79,22 +87,56 @@ namespace PuntoDeVentaV2
                 usuarioActual = $"EMPLEADO: {usuarioActivo}";
             }
 
-            var datosEncabezado = new List<DatosEncabezadoReportes>();
+            decimal totalCantidad = 0;
+            decimal totalEfectivo = 0;
+            decimal totalTarjeta = 0;
+            decimal totalVales = 0;
+            decimal totalCheque = 0;
+            decimal totalTransferencia = 0;
+            decimal totalCredito = 0;
+            decimal totalAnticipo = 0;
 
-            datosEncabezado.Add(new DatosEncabezadoReportes { 
+            if (reporteCajaDT.Rows.Count > 0)
+            {
+                foreach (DataRow cantidades in reporteCajaDT.Rows)
+                {
+                    totalCantidad += Convert.ToDecimal(cantidades["Cantidad"].ToString());
+                    totalEfectivo += Convert.ToDecimal(cantidades["Efectivo"].ToString());
+                    totalTarjeta += Convert.ToDecimal(cantidades["Tarjeta"].ToString());
+                    totalVales += Convert.ToDecimal(cantidades["Vales"].ToString());
+                    totalCheque += Convert.ToDecimal(cantidades["Cheque"].ToString());
+                    totalTransferencia += Convert.ToDecimal(cantidades["Transferencia"].ToString());
+                    totalCredito += Convert.ToDecimal(cantidades["Credito"].ToString());
+                    totalAnticipo += Convert.ToDecimal(cantidades["Anticipo"].ToString());
+                }
+            }
+
+            var datosEncabezado = new List<DatosEncabezadoReportes>();
+            var datosCantidadesTotales = new List<DatosTotalesReporte>();
+
+            datosEncabezado.Add(new DatosEncabezadoReportes
+            {
                 TituloReporte = "REPORTE DE CAJA",
                 Usuario = usuarioActual,
                 Subtitulo = Operacion.ToUpper(),
                 TextoExtra = $"CONSULTA REALIZADA\nDEL {PrimeraFecha} AL {SegundaFecha}",
-                FechaCreacion = $"Fecha de creación: {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}"
+                FechaCreacion = $"Fecha de creación: {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}",
+            });
+
+            datosCantidadesTotales.Add(new DatosTotalesReporte
+            {
+                TotalCantidad = totalCantidad,
+                TotalEfectivo = totalEfectivo,
+                TotalTarjeta = totalTarjeta,
+                TotalVales = totalVales,
+                TotalCheque = totalCheque,
+                TotalTransferencia = totalTransferencia,
+                TotalCredito = totalCredito,
+                TotalAnticipo = totalAnticipo
             });
 
             //========================================================================
 
-            MySqlDataAdapter reporteCajaDA = new MySqlDataAdapter(queryReporteCaja, conn);
-            DataTable reporteCajaDT = new DataTable();
-
-            reporteCajaDA.Fill(reporteCajaDT);
 
             this.reportViewer1.ProcessingMode = ProcessingMode.Local;
             this.reportViewer1.LocalReport.ReportPath = FullReportPath;
@@ -102,9 +144,11 @@ namespace PuntoDeVentaV2
 
             ReportDataSource rp1 = new ReportDataSource("DTReporteCaja", reporteCajaDT);
             ReportDataSource rp2 = new ReportDataSource("DTEncabezado", datosEncabezado);
+            ReportDataSource rp3 = new ReportDataSource("DTTotales", datosCantidadesTotales);
 
             this.reportViewer1.LocalReport.DataSources.Add(rp1);
             this.reportViewer1.LocalReport.DataSources.Add(rp2);
+            this.reportViewer1.LocalReport.DataSources.Add(rp3);
             this.reportViewer1.RefreshReport();
         }
     }
