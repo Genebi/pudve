cb82709454cbaa3982319780d382e2e8f10407da
AndoniSG14 <laletrabeto@gmail.com>
2022-12-01 10:39:36 -0600

    Se implemento que ahora no se dupliquen los productos si uno tiene descuento

--- PuntoDeVentaV2/Ventas.cs
+++ PuntoDeVentaV2/Ventas.cs
@@ -1478,6 +1478,7 @@ namespace PuntoDeVentaV2
 
                             if (resultado == DialogResult.OK)
                             {
+                                enviarVenta.Clear();
                                 DGVentas.Rows[celdaCellClick].Cells["Descuento"].Value = formDescuento.TotalDescuento;
                                 DGVentas.Rows[celdaCellClick].Cells["TipoDescuento"].Value = formDescuento.TipoDescuento;
                             }
@@ -1495,10 +1496,11 @@ namespace PuntoDeVentaV2
                             }
                         }
 
-                        
-                        
+
+
                         if (yasemando.Equals(false))
                         {
+                            enviarVenta.Clear();
                             string Folio = Contenido;
                             string datosCorreoVenta = formaDePagoDeVenta + "|" + cliente + "|" + Folio;
                             foreach (DataGridViewRow articulo in DGVentas.Rows)
@@ -4255,6 +4257,7 @@ namespace PuntoDeVentaV2
                     {
                         if (correoVenta == 1 || correoDescuento == 1)
                         {
+                            enviarVenta.Clear();
                             foreach (DataGridViewRow articulo in DGVentas.Rows)
                             {
                                 enviarVenta.Add(articulo.Cells["Cantidad"].Value.ToString() + "|" + articulo.Cells["Precio"].Value.ToString() + "|" + articulo.Cells["Descripcion"].Value.ToString() + "|" + articulo.Cells["Descuento"].Value.ToString() + "|" + articulo.Cells["Importe"].Value.ToString() + "|" + datosCorreoVenta + "|" + cAnticipo.Text.Trim() + "|" + cAnticipoUtilizado.Text.Trim() + "|" + cDescuento.Text.Trim());
