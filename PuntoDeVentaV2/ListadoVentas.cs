4436f5a0ed43b01314b2fd44048f5f617268c229
AndoniSG14 <laletrabeto@gmail.com>
2023-01-02 17:21:03 -0600

    Se implemento que ahora puedas cancelar las ventas a credito aun que no tengas el suficiente dinero en caja

--- PuntoDeVentaV2/ListadoVentas.cs
+++ PuntoDeVentaV2/ListadoVentas.cs
@@ -1853,13 +1853,16 @@ namespace PuntoDeVentaV2
 
                         if (mensaje == DialogResult.Yes)
                         {
-                            using (var DtTotal = cn.CargarDatos($"SELECT Total FROM Ventas WHERE IDusuario = '{FormPrincipal.userID}' AND ID = '{idVenta}'"))
+                            if (!cbTipoVentas.SelectedIndex.Equals(3))
                             {
-                                decimal Dinero = Convert.ToDecimal(DtTotal.Rows[0]["Total"]);
-                                if (Dinero > CajaN.cantidadTotalEfectivoSaldoInicial)
+                                using (var DtTotal = cn.CargarDatos($"SELECT Total FROM Ventas WHERE IDusuario = '{FormPrincipal.userID}' AND ID = '{idVenta}'"))
                                 {
-                                    MessageBox.Show("No se cuenta con suficiente Efectivo en caja","Avido del Sistema",MessageBoxButtons.OK,MessageBoxIcon.Warning);
-                                    return;
+                                    decimal Dinero = Convert.ToDecimal(DtTotal.Rows[0]["Total"]);
+                                    if (Dinero > CajaN.cantidadTotalEfectivoSaldoInicial)
+                                    {
+                                        MessageBox.Show("No se cuenta con suficiente Efectivo en caja", "Avido del Sistema", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                                        return;
+                                    }
                                 }
                             }
                             var statusVentaParaCancelar = 0;
