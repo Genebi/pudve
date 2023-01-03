450e34988e5a15f9f99c53a8177468e881a6e73f
Alejandro <genebi@outlook.com>
2023-01-02 21:58:30 -0600

    Mandar a imprimir ticket de renta global

--- PuntoDeVentaV2/VerTicketPresupuesto8cmListadoVentas.cs
+++ PuntoDeVentaV2/VerTicketPresupuesto8cmListadoVentas.cs
@@ -264,7 +264,7 @@ namespace PuntoDeVentaV2
             string pathApplication = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
             string rutaTicket = @"PresupuestoRealizado\ReporteTicketPresupuesto80mm.rdlc";
 
-            if (tipoImpresion.Equals(5))
+            if (tipoImpresion.Equals(5) || tipoImpresion.Equals(10))
             {
                 rutaTicket = @"VentaGlobal\VentaGlobal.rdlc";
             }
@@ -303,7 +303,7 @@ namespace PuntoDeVentaV2
             #region Impresion Ticket de 8 cm (80 mm)
             var nombreXSD = "TicketPresupuesto";
 
-            if (tipoImpresion.Equals(5))
+            if (tipoImpresion.Equals(5) || tipoImpresion.Equals(10))
             {
                 nombreXSD = "DTTicketVentaGlobal";
             }
@@ -405,6 +405,14 @@ namespace PuntoDeVentaV2
             reportParameters.Add(new ReportParameter("PathBarCode", pathBarCodeFull));
             //19 parametro integer para mostrar / ocultar Referencia
             reportParameters.Add(new ReportParameter("Referencia", Referencia.ToString()));
+            //20 parametro para el usuario
+            reportParameters.Add(new ReportParameter("Usuario", FormPrincipal.userNickName.ToString()));
+
+            if (tipoImpresion.Equals(5) || tipoImpresion.Equals(10))
+            {
+                //21 parametro para el tipo de ticket
+                reportParameters.Add(new ReportParameter("TipoTicket", tipoImpresion.ToString()));
+            }
 
             LocalReport rdlc = new LocalReport();
             rdlc.EnableExternalImages = true;
