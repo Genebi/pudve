70cd5e6f7166003a201a9b2edceb293c486409cd
Aaron <aaronaguilar12721@gmail.com>
2023-01-05 15:55:56 -0600

    adds

---
+++ PuntoDeVentaV2/creditoVerificacion.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace PuntoDeVentaV2
+{
+    partial class creditoVerificacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(creditoVerificacion));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtVerificar = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(5, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(344, 51);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Introduzca la clave en el mensaje de confirmación ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Clave recibida:";
+            // 
+            // txtVerificar
+            // 
+            this.txtVerificar.Location = new System.Drawing.Point(134, 85);
+            this.txtVerificar.Name = "txtVerificar";
+            this.txtVerificar.Size = new System.Drawing.Size(215, 20);
+            this.txtVerificar.TabIndex = 3;
+            this.txtVerificar.TextChanged += new System.EventHandler(this.txtVerificar_TextChanged);
+            this.txtVerificar.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtVerificar_KeyDown);
+            // 
+            // creditoVerificacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 134);
+            this.Controls.Add(this.txtVerificar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "creditoVerificacion";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Verificación";
+            this.Load += new System.EventHandler(this.creditoVerificacion_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtVerificar;
+    }
+}
