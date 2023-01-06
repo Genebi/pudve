d0d0cc3a30957fbb7dd02784f5414ac51469223c
Aaron <aaronaguilar12721@gmail.com>
2023-01-05 19:29:17 -0600

    .

---
+++ PuntoDeVentaV2/visualizadorAbonoPrimero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PuntoDeVentaV2
+{
+    public partial class visualizadorAbonoPrimero : Form
+    {
+        public visualizadorAbonoPrimero()
+        {
+            InitializeComponent();
+        }
+
+        private void visualizadorAbonoPrimero_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
