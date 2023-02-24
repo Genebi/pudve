c10b14ae8810bdb5fcf74271056b21c0bfc98799
AndoniSG14 <laletrabeto@gmail.com>
2023-02-24 10:34:29 -0600

    Se implemento que ya no puedas abrir mensajes en agregar productos en mensaje ventas y inventario

--- PuntoDeVentaV2/Productos.cs
+++ PuntoDeVentaV2/Productos.cs
@@ -562,6 +562,7 @@ namespace PuntoDeVentaV2
                     }
                 }
             }
+            idprodDobleClick = 0;
         }
 
         // Unhighlight the currently highlighted row.
@@ -684,6 +685,7 @@ namespace PuntoDeVentaV2
 
                 HighlightedRowIndex = -1;
             }
+            dobleClickProducto = 0;
         }
 
         private void btnFilterSearch_Click(object sender, EventArgs e)
